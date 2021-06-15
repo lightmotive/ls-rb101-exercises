5c91575b781b1a3127203874d5028f7b985ebbf7
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-06-15 20:13:25 +0000

    Complete Easy 1/exercise 4

---
+++ 02_easy1/04_how_many.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+# Problem statement: Write a method that counts the number of occurrences of each element in a given array.
+
+# Pseudocode
+
+# Given an array of objects
+#
+# SET hash_counter to empty hash
+#
+# FOR each string in array
+#   SET current_symbol to object symbol representation
+#   SET count_new to 0
+#   SET symbol_count to current_symbol value in hash_counter
+#   IF symbol_count exists
+#     SET count_new = symbol_count
+#   ENDIF
+#   INCREMENT count_new
+#   SET current_symbol value in hash_counter to count_new
+# ENDFOR
+
+def count_occurrences(array)
+  hash_counter = {}
+
+  array.each do |e|
+    current_symbol = e.to_sym
+    count_new = 0
+    symbol_count = hash_counter[current_symbol]
+    count_new = symbol_count unless symbol_count.nil?
+    count_new += 1
+    hash_counter[current_symbol] = count_new
+  end
+
+  hash_counter
+end
+
+vehicles = %w[
+  car car truck car SUV truck
+  motorcycle motorcycle car truck
+]
+
+p count_occurrences(vehicles)
