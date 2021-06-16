8b9d27f01650a26abae6e8811363f7c6e245b975
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-06-16 14:25:14 +0000

    Modify Easy 1/exercise 8
    - Further exploration: update method to return Float

--- 02_easy1/08_array_average.rb
+++ 02_easy1/08_array_average.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 def average(int_array)
-  int_array.sum / int_array.length
+  int_array.sum.to_f / int_array.length
 end
 
-puts average([1, 6]) == 3 # integer division: (1 + 6) / 2 -> 3
-puts average([1, 5, 87, 45, 8, 8]) == 25
-puts average([9, 47, 23, 95, 16, 52]) == 40
+puts average([1, 6]) == (1 + 6).to_f / 2
+puts average([1, 5, 87, 45, 8, 8]) == (1 + 5 + 87 + 45 + 8 + 8).to_f / 6
+puts average([9, 47, 23, 95, 16, 52]) == (9 + 47 + 23 + 95 + 16 + 52).to_f / 6
