ad7334af5dcaae3daf302431295d02f2bca610e7
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-07-06 00:22:29 +0000

    Easy 2 exercise 1 further exploration

--- 03_easy2/01_how_old_is_teddy.rb
+++ 03_easy2/01_how_old_is_teddy.rb
@@ -1,3 +1,7 @@
 # frozen_string_literal: true
 
-puts "Teddy is #{rand(20..200)} years old!"
+puts "=> What's the name?"
+name = gets.strip
+name = 'Teddy' if name.empty?
+
+puts "#{name} is #{rand(20..200)} years old!"
