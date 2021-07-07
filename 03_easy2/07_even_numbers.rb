4c0541938291d574c19da0be2da85b5a13dbe4e3
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-07-07 19:24:24 +0000

    Complete Easy 2 exercise 7

---
+++ 03_easy2/07_even_numbers.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+# Problem statement: Print all even numbers from 1 to 99, inclusive, to the console, with each number on a separate line.
+
+# Pseudocode:
+
+# number = 2
+#
+# WHILE number < 99
+#   PRINT number on a separate line
+#   INCREMENT number by 2
+# ENDWHILE
+
+number = 2
+
+while number <= 98
+  puts number
+  number += 2
+end
