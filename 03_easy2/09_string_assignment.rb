8b7c72e2cd474de999257cd79f722ce2e2ac2086
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-07-12 19:53:41 +0000

    Complete Easy 2 exercise 9

--- 03_easy2/09_string_assignment.rb
+++ 03_easy2/09_string_assignment.rb
@@ -2,7 +2,10 @@
 
 # What does this print out? Can you explain the results?
 
-name = 'Bob'
+name = String.new('Bob')
 save_name = name
+# At this point, name and save_name point to the same object (the object_id would be the same).
 name.upcase!
+# String#upcase! mutates the underlying object.
 puts name, save_name
+# name and save_name will both be "BOB" because they point to the same (mutated) object.
