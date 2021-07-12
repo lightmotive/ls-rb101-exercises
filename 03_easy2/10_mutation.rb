ffe0952f110a790ca300760dbfa17c54dd08f1e3
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-07-12 20:02:17 +0000

    Complete Easy 2 exercise 10

--- 03_easy2/10_mutation.rb
+++ 03_easy2/10_mutation.rb
@@ -5,5 +5,10 @@
 array1 = %w[Moe Larry Curly Shemp Harpo Chico Groucho Zeppo]
 array2 = []
 array1.each { |value| array2 << value }
+# That line adds each value in array1 to array2. The element objects in array2 are identical to those in array1
+# (they point to the same objects in memory).
 array1.each { |value| value.upcase! if value.start_with?('C', 'S') }
-puts array2
+# String#upcase! mutates the underlying object.
+p array2
+# => ["Moe", "Larry", "CURLY", "SHEMP", "Harpo", "CHICO", "Groucho", "Zeppo"]
+# Mutating the strings in array1 affected the values in array2 because they pointed to the same underlying objects.
