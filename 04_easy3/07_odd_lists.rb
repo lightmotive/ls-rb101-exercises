574e76fb3f984cab42e13c5b94f3f0652e476768
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-07-13 21:50:44 +0000

    Further exploration: Easy 3 exercise 7

--- 04_easy3/07_odd_lists.rb
+++ 04_easy3/07_odd_lists.rb
@@ -1,7 +1,27 @@
 # frozen_string_literal: true
 
 def oddities(array)
-  array.select.with_index { |e, idx| idx.even? ? e : nil }
+  odd_elements = []
+  index = 0
+  while index < array.size
+    odd_elements << array[index]
+    index += 2
+  end
+  odd_elements
+
+  # Declarative solution (likely less performant): array.select.with_index { |e, idx| idx.even? ? e : nil }
+end
+
+def conformities(array)
+  even_elements = []
+  index = 1
+  while index < array.size
+    even_elements << array[index]
+    index += 2
+  end
+  even_elements
+
+  # Declarative solution (likely less performant): array.select.with_index { |e, idx| idx.odd? ? e : nil }
 end
 
 p oddities([2, 3, 4, 5, 6]) == [2, 4, 6]
@@ -9,3 +29,10 @@ p oddities([1, 2, 3, 4, 5, 6]) == [1, 3, 5]
 p oddities(%w[abc def]) == ['abc']
 p oddities([123]) == [123]
 p oddities([]) == []
+p oddities([nil]) == [nil]
+
+p conformities([2, 3, 4, 5, 6]) == [3, 5]
+p conformities([1, 2, 3, 4, 5, 6]) == [2, 4, 6]
+p conformities(%w[abc def]) == ['def']
+p conformities([123]) == []
+p conformities([]) == []
