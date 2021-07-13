b5e9aca49896665eff0f45eb1f46a4702abca38e
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-07-13 22:27:04 +0000

    Complete Easy 3 exercise 9
    - Add method that ignores case and non-word chars.

--- 04_easy3/08_palindromic_strings.rb
+++ 04_easy3/08_palindromic_strings.rb
@@ -4,9 +4,20 @@ def palindrome?(implements_reverse)
   implements_reverse == implements_reverse.reverse
 end
 
+def real_palindrome?(string)
+  palindrome?(string.gsub(/\W/, '').downcase)
+end
+
 p palindrome?('madam') == true
 p palindrome?('Madam') == false          # (case matters)
 p palindrome?("madam i'm adam") == false # (all characters matter)
 p palindrome?('356653') == true
 p palindrome?([1, 2, 3, 2, 1]) == true
 p palindrome?([1, 2, 3, 4, 5]) == false
+
+p real_palindrome?('madam') == true
+p real_palindrome?('Madam') == true           # (case does not matter)
+p real_palindrome?("Madam, I'm Adam") == true # (only alphanumerics matter)
+p real_palindrome?('356653') == true
+p real_palindrome?('356a653') == true
+p real_palindrome?('123ab321') == false
