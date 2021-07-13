0e7d9efd176d70ac765e4c035d4e22c770984945
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-07-13 18:55:49 +0000

    Add basic error handling to Easy 3 exercise 2

--- 04_easy3/02_arithmetic_integer.rb
+++ 04_easy3/02_arithmetic_integer.rb
@@ -15,5 +15,9 @@ number1 = prompt_number('Enter the first number')
 number2 = prompt_number('Enter the second number')
 
 OPERATIONS.each do |operation|
-  puts "#{number1} #{operation} #{number2} = #{number1.send(operation, number2)}"
+  result = number1.send(operation, number2)
+rescue StandardError => e
+  result = "Error: #{e.message}"
+ensure
+  puts "#{number1} #{operation} #{number2} = #{result}"
 end
