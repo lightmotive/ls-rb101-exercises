bb280ebd1897853fc0a49c91422a6b53e14dea36
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-07-13 19:59:21 +0000

    Further exploration with Easy 3 exercise 5
    - Add exponentiation with support for zero and negative exponents

--- 04_easy3/05_squaring_an_argument.rb
+++ 04_easy3/05_squaring_an_argument.rb
@@ -8,4 +8,19 @@ def square(arg)
   multiply(arg, arg)
 end
 
+def exponentiate(base, exponent)
+  return 1.to_r if exponent.zero?
+
+  exponentiated = 1
+  exponent.abs.times { exponentiated = multiply(exponentiated, base) }
+
+  return (1.to_r / exponentiated) if exponent.negative?
+
+  exponentiated.to_r
+end
+
 p square(2)
+p exponentiate(2, 3)
+p exponentiate(2, -3)
+p exponentiate(2, -5).to_f
+p exponentiate(2, 0).to_i
