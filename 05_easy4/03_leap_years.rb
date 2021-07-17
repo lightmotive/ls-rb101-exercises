67c6b944332bf6afc786f1334298ec6b7fdaa090
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-07-17 20:18:33 +0000

    Complete Easy 4 exercise 4 - Learp Years, part 2
    - Update logic to apply to British Empire (Julian Calendar before 1752)

--- 05_easy4/03_leap_years.rb
+++ 05_easy4/03_leap_years.rb
@@ -3,7 +3,12 @@
 # Understand the problem
 # - Rules:
 #   - Year is greater than 0
-#   - Leap years:
+#   - Leap years before 1752:
+#     - Include all years evenly divisible by 4.
+#     - The British Empire adopted the Gregorian Calendar in 1752, which was a leap year. Prior to 1752,
+#       the Julian Calendar was used. Under the Julian Calendar, leap years occur in any year that is
+#       evenly divisible by 4.
+#   - Leap years starting with 1752:
 #     - Are evenly divisible by 4
 #     - Are evenly divisible by 400
 #     - Are not evenly divisible by 100 unless also evenly divisible by 400
@@ -14,7 +19,12 @@
 def leap_year?(year)
   raise StandardError, 'The year must be greater than 0.' unless year.positive?
 
-  (year % 400).zero? || ((year % 4).zero? && !(year % 100).zero?)
+  if year > 1752
+    (year % 400).zero? || ((year % 4).zero? && !(year % 100).zero?)
+  else
+    (year % 4).zero?
+  end
+  # A ternary above would work at the expense of readability.
 end
 
 p leap_year?(2016) == true
@@ -26,7 +36,7 @@ p leap_year?(240_001) == false
 p leap_year?(2000) == true
 p leap_year?(1900) == false
 p leap_year?(1752) == true
-p leap_year?(1700) == false
+p leap_year?(1700) == true
 p leap_year?(1) == false
-p leap_year?(100) == false
+p leap_year?(100) == true
 p leap_year?(400) == true
