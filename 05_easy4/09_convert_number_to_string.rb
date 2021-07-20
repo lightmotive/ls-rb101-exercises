927abd0fb87479f29d1ea62077f2d9271ad4d381
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-07-20 23:05:34 +0000

    Complete Easy 4 exercise 9 w/ exploration
    - Identify all mutating string methods without bang

---
+++ 05_easy4/09_convert_number_to_string.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+DIGITS = %w[0 1 2 3 4 5 6 7 8 9].freeze
+
+def integer_to_string(integer)
+  string = String.new
+  quotient = integer
+
+  loop do
+    integer, remainder = integer.divmod(10)
+    string.prepend(DIGITS[remainder])
+    break if quotient.zero?
+  end
+
+  string
+end
+
+p integer_to_string(4321) == '4321'
+p integer_to_string(0) == '0'
+p integer_to_string(5000) == '5000'
+
+# Mutating string methods without bang in Ruby v3.0.1:
+#
+# See https://docs.ruby-lang.org/en/master/String.html (or version-specific page),
+# then look for methods that return 'self'.
+# Watch out for exceptions like setbyte.
+# It really requires experience, experimentation, or scanning the Ruby source code for 'str_duplicate'.
