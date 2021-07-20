b5a1e0d9155cb56a7b5aa31f15111aef10e79227
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-07-20 00:31:31 +0000

    Explore Easy 4 exercise 6
    - Add alternative Enumerable#each_with_object solution

--- 05_easy4/06_running_totals.rb
+++ 05_easy4/06_running_totals.rb
@@ -3,6 +3,9 @@
 def running_total(array)
   sum = 0
   array.map { |number| sum += number }
+
+  # Alternative solution (less elegant than map, but good to know about Enumerable#each_with_object):
+  # array.each_with_object([]) { |number, sum_array| sum_array << (sum_array.empty? ? 0 : sum_array.last) + number }
 end
 
 p running_total([2, 5, 13]) == [2, 7, 20]
