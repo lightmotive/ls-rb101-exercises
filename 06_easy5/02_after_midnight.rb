616fdd82c29b858c11f2942847b598483863c25d
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-07-22 22:21:13 +0000

    Complete Easy 5 exercise 3 (After Midnight, pt. 2)

--- 06_easy5/02_after_midnight.rb
+++ 06_easy5/02_after_midnight.rb
@@ -31,6 +31,8 @@ p time_of_day(3000) == '02:00'
 p time_of_day(800) == '13:20'
 p time_of_day(-4231) == '01:29'
 
+puts "\n* Further exploration *"
+
 # Further exploration 3 - assumes UTC-0
 def weekday_and_time_of_day(minutes)
   time_base_sunday = Time.new(2021, 7, 18)
@@ -39,3 +41,34 @@ def weekday_and_time_of_day(minutes)
 end
 
 p weekday_and_time_of_day(-4231) == 'Thursday 01:29'
+
+puts "\n* Part 2 *"
+
+# time_string should be in 24-hour format hh:mm
+def time_string_to_minutes(time_string)
+  hours, minutes = time_string.split(':').map(&:to_i)
+  (hours * MINUTES_PER_HOUR) + minutes
+end
+
+# time_string should be in 24-hour format hh:mm
+def before_midnight(time_string)
+  minutes = time_string_to_minutes(time_string)
+  return 0 if minutes.zero?
+
+  MINUTES_PER_DAY - minutes
+end
+
+# time_string should be in 24-hour format hh:mm
+def after_midnight(time_string)
+  minutes = time_string_to_minutes(time_string)
+  return 0 if minutes == MINUTES_PER_DAY
+
+  minutes
+end
+
+p after_midnight('00:00') == 0
+p before_midnight('00:00') == 0
+p after_midnight('12:34') == 754
+p before_midnight('12:34') == 686
+p after_midnight('24:00') == 0
+p before_midnight('24:00') == 0
