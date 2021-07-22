6753c1be6461858e3a9ed16f104e29362612c9bd
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-07-22 22:57:16 +0000

    Explore Easy 5 exercise 3 (After Midnight, pt. 2)
    - Solve using Time class.

--- 06_easy5/02_after_midnight.rb
+++ 06_easy5/02_after_midnight.rb
@@ -72,3 +72,36 @@ p after_midnight('12:34') == 754
 p before_midnight('12:34') == 686
 p after_midnight('24:00').zero?
 p before_midnight('24:00').zero?
+
+puts "\n* Part 2, Further Exploration *"
+# Solve using Time class
+require 'time'
+
+def time_base(offset_days = 0)
+  seconds_per_day = MINUTES_PER_DAY * SECONDS_PER_MINUTE
+  Time.strptime('00:00', '%H:%M') + (offset_days * seconds_per_day)
+end
+
+# time_string should be in 24-hour format hh:mm
+def before_midnight(time_string)
+  return 0 if time_string == '00:00'
+
+  (time_base(1) - Time.strptime(time_string, '%H:%M')) / SECONDS_PER_MINUTE
+end
+
+# time_string should be in 24-hour format hh:mm
+def after_midnight(time_string)
+  return 0 if time_string == '24:00'
+
+  (Time.strptime(time_string, '%H:%M') - time_base) / SECONDS_PER_MINUTE
+end
+
+p after_midnight('00:00').zero?
+p before_midnight('00:00').zero?
+p after_midnight('12:34') == 754
+p before_midnight('12:34') == 686
+p after_midnight('24:00').zero?
+p before_midnight('24:00').zero?
+
+# Interestingly, solving using Time class was more complete and required reading documentation.
+# However, most real-world apps would benefit from using Date and Time classes.
