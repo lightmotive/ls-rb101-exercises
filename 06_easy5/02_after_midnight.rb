b2417946c049227d283739258d85118336e6f709
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-07-22 22:25:03 +0000

    Update Easy 5 exercise 3 (After Midnight, pt. 2)
    - Fix Rubocop issues

--- 06_easy5/02_after_midnight.rb
+++ 06_easy5/02_after_midnight.rb
@@ -66,9 +66,9 @@ def after_midnight(time_string)
   minutes
 end
 
-p after_midnight('00:00') == 0
-p before_midnight('00:00') == 0
+p after_midnight('00:00').zero?
+p before_midnight('00:00').zero?
 p after_midnight('12:34') == 754
 p before_midnight('12:34') == 686
-p after_midnight('24:00') == 0
-p before_midnight('24:00') == 0
+p after_midnight('24:00').zero?
+p before_midnight('24:00').zero?
