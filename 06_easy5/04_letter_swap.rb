b51d564cf3786bfaaccf41769970e01e318fe291
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-07-24 22:12:37 +0000

    Improve Easy 5 exercise 4
    - Encapsulate swap logic

--- 06_easy5/04_letter_swap.rb
+++ 06_easy5/04_letter_swap.rb
@@ -1,10 +1,12 @@
 # frozen_string_literal: true
 
+def swap_first_and_last(array)
+  array[0], array[-1] = array[-1], array[0]
+  array
+end
+
 def swap(sentence)
-  sentence.split.map do |word|
-    word[0], word[-1] = word[-1], word[0]
-    word
-  end.join(' ')
+  sentence.split.map { |word| swap_first_and_last(word) }.join(' ')
 end
 
 p swap('Oh what a wonderful day it is') == 'hO thaw a londerfuw yad ti si'
