ea289086be82ebaaed3a4f0b7b6c026d6567b895
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-07-26 18:30:15 +0000

    Complete Easy 5 exercise 7 (Letter Counter, pt 2)
    - Update count logic to exclude non-letters (i.e., count only letters).

--- 06_easy5/06_letter_counter.rb
+++ 06_easy5/06_letter_counter.rb
@@ -1,9 +1,8 @@
 # frozen_string_literal: true
 
-# * Logic & pseudocode *
-# Split the sentence into words (words defined specifically as any block of characters without a space),
-# iterate through words, evaluate the length, and add a count of each length to a hash.
-# There's no need to use RegEx because the definition of "word" is simplified.
+# * Logic & pseudocode (updated to count only letter characters in words) *
+# Split the sentence into words (space-separated blocks of text), iterate through words, and add the count of all A-Z
+# and a-z characters to a hash organized by count value.
 
 # Given a string
 #
@@ -11,7 +10,7 @@
 # SET length_count = new hash object with a key default value of 0
 #
 # FOR each word
-#   SET length = word length
+#   SET length = word length (characters only)
 #   length_count[length] = length_count[length] + 1
 # ENDFOR
 
@@ -20,12 +19,12 @@
 def word_sizes(string)
   length_count = Hash.new(0)
 
-  string.split.each { |word| length_count[word.length] += 1 }
+  string.split.each { |word| length_count[word.count('A-Za-z')] += 1 }
 
   length_count
 end
 
-p word_sizes('Four score and seven.') == { 3 => 1, 4 => 1, 5 => 1, 6 => 1 }
-p word_sizes('Hey diddle diddle, the cat and the fiddle!') == { 3 => 5, 6 => 1, 7 => 2 }
-p word_sizes("What's up doc?") == { 6 => 1, 2 => 1, 4 => 1 }
+p word_sizes('Four score and seven.') == { 3 => 1, 4 => 1, 5 => 2 }
+p word_sizes('Hey diddle diddle, the cat and the fiddle!') == { 3 => 5, 6 => 3 }
+p word_sizes("What's up doc?") == { 5 => 1, 2 => 1, 3 => 1 }
 p word_sizes('') == {}
