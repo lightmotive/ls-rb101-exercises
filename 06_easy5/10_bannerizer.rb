8a152f6a3bda139bf63a0ef0f647a983cb96fdfc
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-07-27 23:53:07 +0000

    Refactor Easy 5 exercise 10
    - Address Rubocop issues.
    - Improve naming.

--- 06_easy5/10_bannerizer.rb
+++ 06_easy5/10_bannerizer.rb
@@ -1,27 +1,43 @@
 # frozen_string_literal: true
 
+def padding_length_total(padding_length)
+  2 + (padding_length * 2)
+end
+
 def box_line(side_char, middle_char, middle_length, padding_length, width_limit: 80)
-  padding_length_total = 2 + (padding_length * 2)
+  padding_length_total = padding_length_total(padding_length)
   middle_length = width_limit - padding_length_total unless middle_length < width_limit - padding_length_total
 
   "#{side_char}#{middle_char * (middle_length + (padding_length * 2))}#{side_char}"
 end
 
+def message_line_slice!(message_current, message_original, message_line_width_limit)
+  message_line = message_current.slice!(0, message_line_width_limit)
+  # Possible improvement: avoid splitting words (add space to replace what's wrapped)
+
+  # Add space if last line:
+  if message_line.length < message_line_width_limit && message_original.length > message_line_width_limit
+    message_line << ' ' * (message_line_width_limit - message_line.length)
+  end
+
+  message_line
+end
+
+def message_line_format(edge_char, padding_length, message_current, message_original, width_limit)
+  padding_length_total = padding_length_total(padding_length)
+  message_line_width_limit = width_limit - padding_length_total
+
+  "#{edge_char}#{' ' * padding_length}" \
+  "#{message_line_slice!(message_current, message_original, message_line_width_limit)}" \
+  "#{' ' * padding_length}#{edge_char}"
+end
+
 def message_line(edge_char, padding_length, message, width_limit: 80)
   msg = String.new(message)
-  box_width = 2 + (padding_length * 2)
-  message_line_width_limit = width_limit - box_width
-
   message_wrapped = String.new
 
   loop do
-    message_line = msg.slice!(0, message_line_width_limit)
-    # Last line:
-    if message_line.length < message_line_width_limit && message.length > message_line_width_limit
-      message_line << ' ' * (message_line_width_limit - message_line.length)
-    end
-    # Possible improvement: avoid splitting words (add space to replace what's wrapped)
-    message_wrapped << "#{edge_char}#{' ' * padding_length}#{message_line}#{' ' * padding_length}#{edge_char}"
+    message_wrapped << message_line_format(edge_char, padding_length, msg, message, width_limit)
     break if msg.length.zero?
 
     message_wrapped << "\n"
