ea2b08bae4f54ab96c394058f0d9a577f0a8183c
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-08-22 22:17:48 +0000

    Explore Easy 6 exercise 10
    - Add flip_vertical and flip_horizontal options.

--- 07_easy6/10_right_triangles.rb
+++ 07_easy6/10_right_triangles.rb
@@ -1,10 +1,22 @@
 # frozen_string_literal: true
 
-def triangle(side_length)
-  1.upto(side_length) do |length|
-    puts ('*' * length).rjust(side_length)
+def triangle(side_length, flip_vertical: false, flip_horizontal: true)
+  enum = if flip_vertical
+           side_length.downto(1)
+         else
+           1.upto(side_length)
+         end
+
+  justification = flip_horizontal ? :rjust : :ljust
+
+  enum.each do |length|
+    puts ('*' * length).send(justification, side_length)
   end
 end
 
 triangle(5)
 triangle(9)
+triangle(9, flip_vertical: true)
+
+triangle(5, flip_horizontal: false)
+triangle(5, flip_vertical: true)
