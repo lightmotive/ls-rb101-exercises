caf78de9c7260f7ea0d77697b378c82af96a2ad7
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-08-22 18:25:55 +0000

    Benchmark (extended) Easy 6 exercise 8
    - Increase test data size and iteration count.
    - Add practical notes.

--- 07_easy6/08_find_the_duplicate.rb
+++ 07_easy6/08_find_the_duplicate.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 # Problem statement: find the duplicate number in an array known to have exactly 1 duplicate
-
 # Basic logic: sort array, then iterate and return when current == previous.
 
 require_relative '../../ruby-common/benchmark_report'
@@ -15,8 +14,10 @@ def test_random_array_with_duplicate(element_count)
   [array, test_duplicate_number]
 end
 
-test_large25k = { label: 'large25k' }
-test_large25k[:input], test_large25k[:expected_output] = test_random_array_with_duplicate(25_000)
+test_large100k = { label: 'large100k' }
+test_large100k[:input], test_large100k[:expected_output] = test_random_array_with_duplicate(100_000)
+test_verylarge1m = { label: 'verylarge1m' }
+test_verylarge1m[:input], test_verylarge1m[:expected_output] = test_random_array_with_duplicate(1_000_000)
 
 TESTS = [{ label: 'small1', input: [1, 5, 3, 1], expected_output: 1 },
          { label: 'small2', input: [18, 9, 36, 96, 31, 19, 54, 75, 42, 15,
@@ -29,8 +30,16 @@ TESTS = [{ label: 'small1', input: [1, 5, 3, 1], expected_output: 1 },
                                     85, 87, 51, 17, 66, 20, 28, 26,  2, 22,
                                     40, 23, 71, 62, 73, 32, 43, 24,  4, 56,
                                     7,  34, 57, 74, 45, 11, 88, 67,  5, 58], expected_output: 73 },
-         test_large25k].freeze
+         test_large100k,
+         test_verylarge1m].freeze
 
+# Unsurprisingly, this basic iterative solution was consistently faster than each_with_index with larger data sets.
+# With smaller data sets, it was still faster most of the time.
+#   Both are very fast with average execution times of 0.054547s vs 0.064502s when including a 100k element test.
+#   Both executed in less than a second after adding a 1m element test: average 0.784654s vs 0.936612s.
+#   Results would vary depending on processing power. Results above are for an older 3.5 GHz Intel CPU.
+# I would choose each_with_index when needing to compare elements positionally like this unless one expected very
+# large data sets or consistently high execution volume on a regular basis.
 def find_dup_by_iteration(array)
   last_number = nil
   array.sort!.each do |number|
@@ -77,7 +86,7 @@ run_tests(TESTS, ->(input) { find_dup_by_each_with_index(input) })
 
 # run_tests(TESTS, ->(input) { find_dup_by_index(input) })
 
-benchmark_report(2, 2, TESTS,
+benchmark_report(2, 10, TESTS,
                  [
                    { label: 'Iteration', method: ->(input) { find_dup_by_iteration(input) } },
                    { label: 'Each with Index', method: ->(input) { find_dup_by_each_with_index(input) } }
