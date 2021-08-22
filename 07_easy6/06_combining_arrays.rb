119aa3dbc260b8063931d5b6f59498dbbd7630ab
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-08-22 02:59:06 +0000

    Simplify Easy 6 exercise 6
    - Use Array#inject.
    - Show Ruby v2.7+ solution that uses argument forwarding.

--- 07_easy6/06_combining_arrays.rb
+++ 07_easy6/06_combining_arrays.rb
@@ -1,9 +1,21 @@
 # frozen_string_literal: true
 
+# Ruby v2.7+ solution
+# ===================
+# def merge(...)
+#   [].union(...)
+# end
+
 def merge(*arrays)
-  arrays.each_with_object([]) do |array, merged|
-    array.each { |element| merged << element unless merged.include?(element) }
-  end
+  arrays.inject([]) { |merged, array| merged | array }
 end
+
+# First solution
+# ==============
+# def merge(*arrays)
+#   arrays.each_with_object([]) do |array, merged|
+#     array.each { |element| merged << element unless merged.include?(element) }
+#   end
+# end
 
 p merge([1, 3, 5], [3, 6, 9]) #== [1, 3, 5, 6, 9]
