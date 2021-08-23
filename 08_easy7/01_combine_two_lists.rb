c7b11e230180bb73451893639dc0a6aa0a59bf3f
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-08-23 23:00:50 +0000

    Easy 7 exercise 1: standardize test data

--- 08_easy7/01_combine_two_lists.rb
+++ 08_easy7/01_combine_two_lists.rb
@@ -1,5 +1,18 @@
 # frozen_string_literal: true
 
+require_relative '../../ruby-common/test'
+
+TESTS = [
+  { label: 'two arrays', input: [[1, 2, 3], %w[a b c]], expected_output: [1, 'a', 2, 'b', 3, 'c'] },
+  { label: 'three arrays', input: [[1, 2, 3], %w[a b c], %i[x y z]],
+    expected_output: [1, 'a', :x, 2, 'b', :y, 3, 'c', :z] },
+  { label: 'two empty arrays', input: [[], []], expected_output: [] },
+  { label: 'three arrays with shorter tail', input: [[1, 2, 3], %w[a b c], %i[x y]],
+    expected_output: [1, 'a', :x, 2, 'b', :y, 3, 'c', nil] },
+  { label: 'three arrays with shorter head', input: [[1, 2], %w[a b c], %i[x y z]],
+    expected_output: [1, 'a', 2, 'b', 3, 'c'] }
+].freeze
+
 def interleave(*arrays)
   interleaved = []
   arrays[0].each_index do |index|
@@ -11,7 +24,4 @@ def interleave(*arrays)
   interleaved
 end
 
-p interleave([1, 2, 3], %w[a b c]) == [1, 'a', 2, 'b', 3, 'c']
-p interleave([1, 2, 3], %w[a b c], %i[x y z]) == [1, 'a', :x, 2, 'b', :y, 3, 'c', :z]
-p interleave([], []) == []
-p interleave([1, 2, 3], %w[a b c], %i[x y]) == [1, 'a', :x, 2, 'b', :y, 3, 'c', nil]
+run_tests(TESTS, ->(input) { interleave(*input) })
