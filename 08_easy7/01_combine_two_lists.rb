d992921e4254953d9a1c4574a96012d37aa1a5ce
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-08-23 18:53:27 +0000

    Complete Easy 7 exercise 1

---
+++ 08_easy7/01_combine_two_lists.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+def interleave(*arrays)
+  interleaved = []
+  arrays[0].each_index do |index|
+    arrays.each do |array|
+      interleaved << array[index]
+    end
+  end
+
+  interleaved
+end
+
+p interleave([1, 2, 3], %w[a b c]) == [1, 'a', 2, 'b', 3, 'c']
+p interleave([1, 2, 3], %w[a b c], %i[x y z]) == [1, 'a', :x, 2, 'b', :y, 3, 'c', :z]
+p interleave([], []) == []
