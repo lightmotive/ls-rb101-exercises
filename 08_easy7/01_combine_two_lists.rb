cd25a19400aeefbf09394a70e5b6654b3d54c160
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-08-23 21:34:51 +0000

    Easy 7 exercise 1: add test
    - Confirm nil fill when needed.

--- 08_easy7/01_combine_two_lists.rb
+++ 08_easy7/01_combine_two_lists.rb
@@ -14,3 +14,4 @@ end
 p interleave([1, 2, 3], %w[a b c]) == [1, 'a', 2, 'b', 3, 'c']
 p interleave([1, 2, 3], %w[a b c], %i[x y z]) == [1, 'a', :x, 2, 'b', :y, 3, 'c', :z]
 p interleave([], []) == []
+p interleave([1, 2, 3], %w[a b c], %i[x y]) == [1, 'a', :x, 2, 'b', :y, 3, 'c', nil]
