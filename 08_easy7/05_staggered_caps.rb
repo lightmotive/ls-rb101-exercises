a00a83870167d5895e7dbb5f6013dbd6135da8df
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-08-25 19:53:33 +0000

    Complete Easy 7 exercise 5, part 2
    - Ignore non-alphabetic chars when toggling case.

--- 08_easy7/05_staggered_caps.rb
+++ 08_easy7/05_staggered_caps.rb
@@ -3,12 +3,14 @@
 def staggered_case(string, start_upcase: true)
   upcase = !start_upcase
   string.chars.map do |char|
+    next char if char =~ /[^a-zA-Z]/
+
     upcase = !upcase
     upcase ? char.upcase : char.downcase
   end.join
 end
 
-p staggered_case('I Love Launch School!') == 'I LoVe lAuNcH ScHoOl!'
-p staggered_case('ALL_CAPS') == 'AlL_CaPs'
-p staggered_case('ignore 77 the 444 numbers') == 'IgNoRe 77 ThE 444 NuMbErS'
-p staggered_case('ignore 77 the 444 numbers', start_upcase: false) == 'iGnOrE 77 tHe 444 nUmBeRs'
+p staggered_case('I Love Launch School!') == 'I lOvE lAuNcH sChOoL!'
+p staggered_case('ALL CAPS') == 'AlL cApS'
+p staggered_case('ignore 77 the 444 numbers') == 'IgNoRe 77 ThE 444 nUmBeRs'
+p staggered_case('ignore 77 the 444 numbers', start_upcase: false) == 'iGnOrE 77 tHe 444 NuMbErS'
