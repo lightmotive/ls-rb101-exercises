e44fe4a886423aa724bf14226bd24a032d01ee5e
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-08-25 19:41:52 +0000

    Complete Easy 7 exercise 5

---
+++ 08_easy7/05_staggered_caps.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+def staggered_case(string)
+  upcase = false
+  string.chars.map do |char|
+    upcase = !upcase
+    upcase ? char.upcase : char.downcase
+  end.join
+end
+
+p staggered_case('I Love Launch School!') == 'I LoVe lAuNcH ScHoOl!'
+p staggered_case('ALL_CAPS') == 'AlL_CaPs'
+p staggered_case('ignore 77 the 444 numbers') == 'IgNoRe 77 ThE 444 NuMbErS'
