fba35d97d0319eda77aae4b2641e9d41d1c35180
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-09-01 22:18:00 +0000

    Explore Easy 7 exercise 8: support 3+ array args

--- 08_easy7/08_multiply_lists.rb
+++ 08_easy7/08_multiply_lists.rb
@@ -8,8 +8,9 @@ end
 
 p multiply_list([3, 5, 7], [9, 10, 11]) == [27, 50, 77]
 
-def multiple_list_with_zip(array1, array2)
-  array1.zip(array2).map { |numbers| numbers.inject(&:*) }
+def multiple_list_with_zip(*arrays)
+  arrays[0].zip(*arrays[1, arrays.size - 1]).map { |numbers| numbers.inject(:*) }
 end
 
 p multiple_list_with_zip([3, 5, 7], [9, 10, 11]) == [27, 50, 77]
+p multiple_list_with_zip([3, 5, 7], [9, 10, 11], [12, 13, 14]) == [324, 650, 1078]
