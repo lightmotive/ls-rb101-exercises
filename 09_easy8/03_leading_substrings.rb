22fd722755d36d8553eaaed70b952772b3392796
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-09-04 19:23:55 +0000

    Complete Easy 8 exercise 4: All Substrings

--- 09_easy8/03_leading_substrings.rb
+++ 09_easy8/03_leading_substrings.rb
@@ -15,3 +15,23 @@ end
 p leading_substrings('abc') == %w[a ab abc]
 p leading_substrings('a') == ['a']
 p leading_substrings('xyzzy') == %w[x xy xyz xyzz xyzzy]
+
+# All Substrings (exercise 4)
+def substrings(string)
+  result = []
+
+  string.length.times do |idx|
+    substring = string[idx..-1]
+    result.concat(leading_substrings(substring))
+  end
+
+  result
+end
+
+p(substrings('abcde') == %w[
+  a ab abc abcd abcde
+  b bc bcd bcde
+  c cd cde
+  d de
+  e
+])
