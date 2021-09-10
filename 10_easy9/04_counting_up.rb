71891650b6c32e03b71d921edc199b90afb73d14
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-09-10 19:22:57 +0000

    Explore Easy 9 exercise 4: compare splat w/ to_a
    - Add method with splat operator.
    - Add test data.
    - Add benchmark.

--- 10_easy9/04_counting_up.rb
+++ 10_easy9/04_counting_up.rb
@@ -9,12 +9,36 @@
 #   Would it be better to have specific methods like Ruby's Integer#upto and #downto?
 #   Or would you want another argument that specifies the sequence direction.
 
-def sequence(end_number)
+require_relative '../../ruby-common/test'
+require_relative '../../ruby-common/benchmark_report'
+
+TESTS = [
+  { input: 5, expected_output: [1, 2, 3, 4, 5] },
+  { input: 3, expected_output: [1, 2, 3] },
+  { input: 1, expected_output: [1] },
+  { input: 0, expected_output: [] },
+  { input: -5, expected_output: [-1, -2, -3, -4, -5] },
+  { input: 50_000_000, expected_output: 1.upto(50_000_000).to_a }
+].freeze
+
+def sequence_with_splat(end_number)
+  end_number.positive? ? [*1..end_number] : [*-1.downto(end_number)]
+end
+
+run_tests('splat', TESTS, ->(input) { sequence_with_splat(input) })
+
+def sequence_with_to_a(end_number)
   end_number.positive? ? (1..end_number).to_a : -1.downto(end_number).to_a
 end
 
-p sequence(5) == [1, 2, 3, 4, 5]
-p sequence(3) == [1, 2, 3]
-p sequence(1) == [1]
-p sequence(0) == []
-p sequence(-5) == [-1, -2, -3, -4, -5]
+run_tests('to_a', TESTS, ->(input) { sequence_with_to_a(input) })
+
+benchmark_report(1, 2, TESTS,
+                 [
+                   { label: 'splat', method: ->(input) { sequence_with_splat(input) } },
+                   { label: 'to_a', method: ->(input) { sequence_with_to_a(input) } }
+                 ])
+
+# to_a is consistently around 1.06 - 1.15x faster with larger collections. Probably not going to be a bottleneck in a
+#   typical Ruby application.
+# splat is sometimes faster with small collections.
