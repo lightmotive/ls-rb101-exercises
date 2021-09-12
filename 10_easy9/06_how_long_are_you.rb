0ee1a0afc44e5c46e0cc4fb6667d8b977ccb5eb0
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-09-12 23:26:45 +0000

    Complete Easy 9 exercise 6

---
+++ 10_easy9/06_how_long_are_you.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+# Basic logic:
+# - Split string into words.
+# - Map the words to a new array with the word, space, and length.
+
+def word_lengths(string)
+  string.split.map { |word| "#{word} #{word.length}" }
+end
+
+p word_lengths('cow sheep chicken') == ['cow 3', 'sheep 5', 'chicken 7']
+p word_lengths('baseball hot dogs and apple pie') == ['baseball 8', 'hot 3', 'dogs 4', 'and 3', 'apple 5', 'pie 3']
+p word_lengths("It ain't easy, is it?") == ['It 2', "ain't 5", 'easy, 5', 'is 2', 'it? 3']
+p word_lengths('Supercalifragilisticexpialidocious') == ['Supercalifragilisticexpialidocious 34']
+p word_lengths('') == []
