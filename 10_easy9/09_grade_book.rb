900d8bac5db538ebde0bbeacb72ea5acd88bdb8a
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-09-12 23:47:20 +0000

    Complete Easy 9 exercise 9

---
+++ 10_easy9/09_grade_book.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+def get_grade(*grades)
+  average = grades.sum / grades.count
+
+  case average
+  when 90..100 then 'A'
+  when 80...90 then 'B'
+  when 70...80 then 'C'
+  when 60...70 then 'D'
+  when 0...60 then 'F'
+  end
+end
+
+p get_grade(95, 90, 93) == 'A'
+p get_grade(50, 50, 95) == 'D'
