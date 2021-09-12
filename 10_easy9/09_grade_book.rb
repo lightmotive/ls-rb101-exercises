f914ba7b5e3aa88c983c844968797831a7425ead
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-09-12 23:54:58 +0000

    Explore Easy 9 exercise 9: extra credit grades?

--- 10_easy9/09_grade_book.rb
+++ 10_easy9/09_grade_book.rb
@@ -4,6 +4,7 @@ def get_grade(*grades)
   average = grades.sum / grades.count
 
   case average
+  when 101..Float::INFINITY then 'A+'
   when 90..100 then 'A'
   when 80...90 then 'B'
   when 70...80 then 'C'
@@ -14,3 +15,4 @@ end
 
 p get_grade(95, 90, 93) == 'A'
 p get_grade(50, 50, 95) == 'D'
+p get_grade(99, 100, 107) == 'A+'
