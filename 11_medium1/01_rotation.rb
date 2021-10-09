d8ab9e3a17bdc3d473db2bcdbc64649d8f2e8207
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-10-09 18:40:48 +0000

    Explore Medium 1 exercise 1
    - Add string and integer rotation.

--- 11_medium1/01_rotation.rb
+++ 11_medium1/01_rotation.rb
@@ -11,3 +11,17 @@ p rotate_array(['a']) == ['a']
 x = [1, 2, 3, 4]
 p rotate_array(x) == [2, 3, 4, 1] # => true
 x == [1, 2, 3, 4]                 # => true
+
+# Further exploration
+
+def rotate_string(string)
+  rotate_array(string.chars).join
+end
+
+p rotate_string('abcdef') == 'bcdefa'
+
+def rotate_integer(int)
+  rotate_string(int.to_s).to_i
+end
+
+p rotate_integer(12_345) == 23_451
