b2a5df6f902c7571881645411eeea95f6d5aa2db
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-10-17 19:58:35 +0000

    Explore Medium 1 exercise 5 - Diamonds!
    - Implement outline option.

--- 11_medium1/05_diamonds.rb
+++ 11_medium1/05_diamonds.rb
@@ -7,24 +7,36 @@
 # - Append asterisk rows, increasing by two, until the total asterisks = number.
 # - Copy the first half of the diamond minus the last row, reverse, and append.
 
-def diamond_strings(width)
+def diamond_string(width, count, outline: false)
+  asterisks =
+    if count == 1 then '*'
+    elsif outline then "*#{' ' * (count - 2)}*"
+    else '*' * count
+    end
+
+  asterisks.center(width, ' ')
+end
+
+def diamond_strings(width, outline: false)
   strings = []
   count = 1
 
   while count <= width
-    strings.push(('*' * count).center(width, ' '))
+    strings.push(diamond_string(width, count, outline: outline))
     count += 2
   end
 
   strings.concat(strings[0..-2].reverse)
 end
 
-def diamond_display(width)
-  puts diamond_strings(width)
+def diamond_display(width, outline: false)
+  puts diamond_strings(width, outline: outline)
 end
 
 p diamond_strings(1) == ['*']
+p diamond_strings(1, outline: true) == ['*']
 p diamond_strings(3) == [' * ', '***', ' * ']
+p diamond_strings(3, outline: true) == [' * ', '* *', ' * ']
 p diamond_strings(9) == ['    *    ', '   ***   ', '  *****  ', ' ******* ',
                          '*********', ' ******* ', '  *****  ', '   ***   ',
                          '    *    ']
@@ -33,4 +45,6 @@ puts
 
 diamond_display(1)
 diamond_display(3)
+diamond_display(3, outline: true)
 diamond_display(9)
+diamond_display(9, outline: true)
