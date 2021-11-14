b5b3ef9b737ac6f3c45e7de4a302bece643db07b
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-11-14 23:50:47 +0000

    Implement Medium 2 exercise 2

--- 12_medium2/02_now_i_know_my_abcs.rb
+++ 12_medium2/02_now_i_know_my_abcs.rb
@@ -41,25 +41,37 @@
 DEFAULT_LETTER_SETS = %w[B:O X:K D:Q C:P N:A G:T R:E F:S J:W H:U V:I L:Y Z:M]
 
 def blocks_create(letter_sets)
-  # Create hash from letter_sets using this sample structure:
-  # { 'B:O': { letters: %w[B O], used: false }, ... }
+  letter_sets.map do |letter_set|
+    { letters: letter_set.split(':'), used: false }
+  end
 end
 
-def block(letter, blocks)
-  # Select and return the block from blocks by letter
+def blocks_by_letter(letter, blocks)
+  letter = letter.upcase
+  blocks.select { |block| block[:letters].include?(letter) }
 end
 
-def use_block(letter, block)
-  # Mark the block's :used value to true
+def block_used?(block)
+  block[:used]
+end
+
+def block_use(block)
+  block[:used] = true
 end
 
 def block_word?(word, blocks = nil)
-  # Given a word and a set of blocks (default blocks if not specified):
   blocks = blocks_create(DEFAULT_LETTER_SETS) if blocks.nil?
-  
-  # - Loop through each character
-  #   - Check whether the character exists in a set of blocks where not used.
-  #     - If yes, mark the block as used and continue.
-  #     - If no, stop iterating by returning false.
-  #     - If iteration completes without returning false, return true.
+
+  word.chars.each do |letter|
+    block = blocks_by_letter(letter, blocks).first
+    return false if block.nil? || block_used?(block)
+
+    block_use(block)
+  end
+
+  true
 end
+
+p block_word?('BATCH') == true
+p block_word?('BUTCH') == false
+p block_word?('jest') == true
