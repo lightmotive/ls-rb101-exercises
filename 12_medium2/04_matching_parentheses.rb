1e11c890b2e40464659817b37f0b609d1709050b
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-11-15 20:01:02 +0000

    Explore Medium 2 exercise 4
    - Logically process a new features that supports checking other pairs.
    - Refactor to support basic multi-set implementation.

--- 12_medium2/04_matching_parentheses.rb
+++ 12_medium2/04_matching_parentheses.rb
@@ -8,16 +8,37 @@
 #   - If the value ever falls below 0, return false.
 # - Return true if count == 0.
 
-def balanced?(string)
+# More advanced logic: check for balance with multiple open-close pairs
+# - Return false if one set closes while any other set is open.
+# - Still return false if any set goes negative.
+
+# Even more advanced logic: check for balance with pairs that use the same
+# open-close char, like ' and ".
+# - If the open and close char is the same (check with each evaluation),
+#   subtract 1 if the set is currently 1.
+
+def balanced_set?(string, char_set)
   count = 0
 
   string.each_char do |char|
-    count += 1 if char == '('
-    count -= 1 if char == ')'
+    count += 1 if char == char_set[:open]
+    count -= 1 if char == char_set[:close]
     break if count.negative?
   end
 
   count.zero?
+end
+
+def balanced?(string)
+  char_sets = [
+    { open: '(', close: ')' },
+    { open: '[', close: ']' },
+    { open: '{', close: '}' },
+    { open: '“', close: '”' },
+    { open: '‘', close: '’' }
+  ]
+
+  char_sets.all? { |char_set| balanced_set?(string, char_set) }
 end
 
 p balanced?('What (is) this?') == true
@@ -27,4 +48,12 @@ p balanced?('((What) (is this))?') == true
 p balanced?('((What)) (is this))?') == false
 p balanced?('Hey!') == true
 p balanced?(')Hey!(') == false
-p balanced?('What ((is))) up(') == false
+p balanced?('“What ((is)) up”') == true
+p balanced?('What {[((is))]} up') == true
+p balanced?('What {[((is))] up') == false
+p balanced?('What ‘is’ up’') == false
+p balanced?('[(])') == false
+p balanced?("'syntax error...") == false
+p balanced?('"syntax complete!"') == true
+
+# To solve that, one would need to track balance across all char sets.
