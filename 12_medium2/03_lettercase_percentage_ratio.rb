150fedbd05f591130595a0b5e6225383d238530a
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-11-15 00:44:38 +0000

    Explore Medium 2 exercise 3
    - Round percentages.
    - Rename `ratio` to `percentage`.

--- 12_medium2/03_lettercase_percentage_ratio.rb
+++ 12_medium2/03_lettercase_percentage_ratio.rb
@@ -10,17 +10,18 @@
 #   - Divide those counts by total chars to get percentage, then multiply
 #     by 100 to get ratio.
 
-def ratio(count, total)
-  (count / total.to_f) * 100
+def percentage(count, total)
+  ((count / total.to_f) * 100).round(1)
 end
 
 def letter_percentages(string)
   total = string.size
-  { lowercase: ratio(string.count('a-z'), total),
-    uppercase: ratio(string.count('A-Z'), total),
-    neither: ratio(string.count('^A-Za-z'), total) }
+  { lowercase: percentage(string.count('a-z'), total),
+    uppercase: percentage(string.count('A-Z'), total),
+    neither: percentage(string.count('^A-Za-z'), total) }
 end
 
 p letter_percentages('abCdef 123') == { lowercase: 50.0, uppercase: 10.0, neither: 40.0 }
 p letter_percentages('AbCd +Ef') == { lowercase: 37.5, uppercase: 37.5, neither: 25.0 }
 p letter_percentages('123') == { lowercase: 0.0, uppercase: 0.0, neither: 100.0 }
+p letter_percentages('abcdefGHI') == { lowercase: 66.7, uppercase: 33.3, neither: 0.0 }
