2c256dbe2fb30d43438daa9a13ec8a90f1c253d8
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-11-18 03:34:27 +0000

    Explore Medium 2 exercise 4
    - Use Regexp, including Recursion, to simplify code.

--- 12_medium2/04_matching_parentheses.rb
+++ 12_medium2/04_matching_parentheses.rb
@@ -8,116 +8,85 @@
 #   - If the value ever falls below 0, return false.
 # - Return true if count == 0.
 
-# More advanced logic: check for balanced pairs within pairs so the following
-# tests pass:
+# More robust logic: check for balance within pairs so these tests pass:
 #   - p balanced?('[)]') == false
 #   - p balanced?('[(])([)][]') == false
 #   - p balanced?("'syntax error...") == false
 #   - p balanced?('"syntax complete!"') == true
 #   - p balanced?('content ) [other "content"] ( end') == false
-# A previous implementation used manual pair tracking. A simpler solution might
-# use Regexp by basically looping through strings that reduce in size:
-#   - Find all pairs with content, e.g., /\(.*\)/ (loop through all pairs and
-#     concatenate list).
-#     - Evaluate content before and after matching pairs. This regexp should
-#       match content before the first pair char and after last pair char
-#       (open or close). If the content contains any pair char, it's unbalanced.
-#   - Evaluate each match's content (within pair chars) to ensure all is
-#     balanced within matching pairs.
+# The previous implementation used manual pair tracking (count). A simpler
+# solution could use Regex to find matching pairs, remove the matching brackets,
+# and repeat until there are no more matching pairs.
+#   - If any match within matching pair chars is unbalanced, return false.
+#   - Replace the matching pair chars, then continue evaluating.
+#   - When matches are empty, check if the string still contains match chars.
+#     - If so, return false.
+#     - Otherwise, return true.
 
-# Even more advanced logic: check for balance with pairs that use the same
-# open-close char, like ' and ".
-# - If the open and close char is the same (check with each evaluation),
-#   subtract 1 if the set is currently 1.
+PAIRS = [%w[( )], %w([ ]), %w[{ }],
+         ['\u201C', '\u201D'], ['\u2018', '\u2019'],
+         %w[' '], %w[" "]].freeze
 
-PAIRS = %w[() [] {} “” ‘’].freeze
+def regexp_set_escaped(chars)
+  chars.map do |char|
+    next "\\#{char}" if %w([ ]).include?(char)
 
-def pairs_initialize(pairs = PAIRS)
-  pairs.map do |set|
-    { open: set[0], close: set[1], count: 0, close_before_pairs: [] }
+    char
   end
 end
 
-def pairs_open(pairs, except_set: nil)
-  pairs_open = pairs.select { |set| set[:count].positive? }
-  return pairs_open if except_set.nil?
+# Get [open_chars, close_chars]
+def pairs_open_close_partitioned
+  chars = PAIRS.flatten
+  chars.partition.with_index { |_, idx| idx.even? }
+end
 
-  pairs_open.reject do |set|
-    set[:open] == except_set[:open]
+PAIR_CHARS_SET_REGEXP =
+  Regexp.new("[#{regexp_set_escaped(PAIRS.flatten).join}]")
+
+def pair_char_for_regexp(char)
+  return char if char.start_with?('\u')
+
+  Regexp.escape(char)
+end
+
+# This method uses Regexp Recursion (\g<0>) to find matching pairs.
+# Note: some language Regex implementations use (?R) or (?0).
+# See https://www.regular-expressions.info/recurse.html for more details.
+def pair_match_regexp_strings
+  PAIRS.map do |pair|
+    open_char, close_char = pair
+    boundary_start = pair_char_for_regexp(open_char)
+    content = "(?>[^#{regexp_set_escaped(pair).join}]|(\\g<0>))*"
+    boundary_end = pair_char_for_regexp(close_char)
+    "#{boundary_start}#{content}#{boundary_end}"
   end
 end
 
-def update_pairs_open!(set, pairs)
-  set[:count] += 1
-  other_pairs_open = pairs_open(pairs, except_set: set)
-  return if set[:count] != 1 || other_pairs_open.empty?
-
-  other_pairs_open.each do |open_set|
-    set[:close_before_pairs].push(
-      { close: open_set[:close], count_min: open_set[:count] }
-    )
-  end
+def pair_matches_regexp
+  Regexp.new("(#{pair_match_regexp_strings.join('|')})")
 end
 
-def update_pairs_close!(set)
-  set[:count] -= 1
-  set[:close_before_pairs].clear if set[:count].zero?
-end
+PAIR_MATCHES_REGEXP = pair_matches_regexp
 
-def update_pairs!(char, pairs)
-  set_arr = pairs.select { |set| [set[:open], set[:close]].include?(char) }
-  return if set_arr.empty?
-
-  set = set_arr.first
-
-  update_pairs_open!(set, pairs) if char == set[:open]
-  update_pairs_close!(set) if char == set[:close]
-
-  nil
-end
-
-def pairs_trapped(pairs)
-  pairs.select do |set|
-    close_before_pairs = set[:close_before_pairs]
-    next false if close_before_pairs.empty?
-
-    close_before_pairs.any? do |close_before_set|
-      pairs.any? do |set2|
-        set2[:close] == close_before_set[:close] &&
-          set2[:count] < close_before_set[:count_min]
-      end
-    end
-  end
-end
-
-def char_set_trapped?(pairs)
-  !pairs_trapped(pairs).empty?
-end
-
-def pairs_broken?(pairs)
-  return true if pairs.any? { |set| set[:count].negative? }
-  return true if char_set_trapped?(pairs)
-
-  false
-end
-
-def pairs_balanced?(pairs)
-  return false if pairs.any? { |set| set[:count] != 0 }
-
-  true
+def pair_matches(string)
+  string.scan(PAIR_MATCHES_REGEXP).flatten.compact.reject(&:empty?).uniq
 end
 
 def balanced?(string)
-  pairs = pairs_initialize
+  loop do
+    matches = pair_matches(string)
+    break if matches.empty?
 
-  string.each_char do |char|
-    # - Return false if one set closes while any other set is open.
-    # - Still return false if any set goes negative.
-    update_pairs!(char, pairs)
-    break if pairs_broken?(pairs)
+    matches.each do |match|
+      replacement = match[1..-2]
+      return false unless balanced?(replacement)
+
+      string = string.gsub(match, replacement)
+    end
   end
 
-  pairs_balanced?(pairs)
+  string =~ PAIR_CHARS_SET_REGEXP ? false : true
 end
 
 p balanced?('What (is) this?') == true
@@ -127,11 +96,20 @@ p balanced?('((What) (is this))?') == true
 p balanced?('((What)) (is this))?') == false
 p balanced?('Hey!') == true
 p balanced?(')Hey!(') == false
+p balanced?(')(Hey!)(') == false
 p balanced?('“What ((is)) up”') == true
 p balanced?('What {[((is))]} up') == true
 p balanced?('What {[((is))] up') == false
 p balanced?('What ‘is’ up’') == false
-p balanced?('[(])') == false
+p balanced?('[(hello))])') == false
 p balanced?('[(]') == false
-p balanced?('[)]') == false
+p balanced?('[(()(]') == false
+p balanced?('([[])]') == false
 p balanced?('[(])([)][]') == false
+p balanced?("'syntax error...") == false
+p balanced?('"syntax correct"') == true
+p balanced?('content ) [other "content"] ( end )') == false
+p balanced?('content { [other "content"] } end }') == false
+p balanced?('content [ (other ( "content") ] end') == false
+p balanced?('(What)(What(What))((What)') == false
+p balanced?('(What)(What(What))(What)') == true
