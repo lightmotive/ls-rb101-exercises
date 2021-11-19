62f14bddf07b981cab8ffc6a61303f96ce091486
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-11-19 22:44:18 +0000

    Logically process Medium 2 exercise 7, basic logic
    - "Unlucky Days"

---
+++ 12_medium2/07_unlucky_days.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+# Basic logic
+# - Initialize two Date objects.
+#   - Create a range from day 1 through 12/31 of specified year.
+# - Call `select` on range.
+#    - Within `select`, return true when day of month is 13 and day of week
+#      is 5.
