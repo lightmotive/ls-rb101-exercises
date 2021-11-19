3296fadc595c7378897f6c2e5b28299fd86357d9
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-11-19 20:53:53 +0000

    Complete Medium 2 exercise 6, "Tri-Angles"

--- 12_medium2/06_tri_angles.rb
+++ 12_medium2/06_tri_angles.rb
@@ -33,3 +33,18 @@
 # - Return :right if angles includes 90
 # - Return :acute if all angles < 90
 # - Return :obtuse if angles includes any value > 90
+
+def triangle(*angles)
+  return :invalid if angles.include?(0) || angles.sum != 180
+
+  if angles.include?(90) then :right
+  elsif angles.one? { |angle| angle > 90 } then :obtuse
+  else :acute
+  end
+end
+
+p triangle(60, 70, 50) == :acute
+p triangle(30, 90, 60) == :right
+p triangle(120, 50, 10) == :obtuse
+p triangle(0, 90, 90) == :invalid
+p triangle(50, 50, 50) == :invalid
