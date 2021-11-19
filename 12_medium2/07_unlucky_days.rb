25c90f12e033f43889bb6af0fc74d7f06a309cf7
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-11-19 23:20:11 +0000

    Improve Medium 2 exercise 7
    - Select from even smaller sets: month day 13, then Fridays.

--- 12_medium2/07_unlucky_days.rb
+++ 12_medium2/07_unlucky_days.rb
@@ -4,8 +4,8 @@
 # - Initialize two Date objects.
 #   - Create a range from day 1/13 through 12/13 of specified year.
 # - Call `select` on range.
-#   - First, select Thursdays.
-#   - Next, select where month day = 13.
+#   - First, select month day 13.
+#   - Next, select Fridays.
 
 require 'date'
 
@@ -13,7 +13,7 @@ def friday_13th_dates(year)
   date_start = Date.new(year, 1, 13)
   date_end = Date.new(year, 12, 13)
 
-  (date_start..date_end).select(&:friday?).select { |date| date.mday == 13 }
+  (date_start..date_end).select { |date| date.mday == 13 }.select(&:friday?)
 end
 
 def friday_13th_count(year)
