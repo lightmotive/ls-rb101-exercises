e1d97365d05800afe952562807ef6be2c46e70dc
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-11-19 22:59:09 +0000

    Complete Medium 2 exercise 7

--- 12_medium2/07_unlucky_days.rb
+++ 12_medium2/07_unlucky_days.rb
@@ -7,18 +7,19 @@
 #    - Within `select`, return true when day of month is 13 and day of week
 #      is 5.
 
+require 'date'
+
 def friday_13th_dates(year)
-  # - Initialize two Date objects.
-  #   - Create a range from day 1 through 12/31 of specified year.
-  # - Call `select` on range.
-  #    - Within `select`, return true when day of month is 13 and day of week
-  #      is 5.
+  date_start = Date.new(year, 1, 1)
+  date_end = Date.new(year, 12, 31)
+
+  (date_start..date_end).select { |date| date.wday == 5 && date.mday == 13 }
 end
 
 def friday_13th_count(year)
-  friday_13th_dates(year).size
+  friday_13th_dates(year).tap { |dates| p dates }.size
 end
 
-friday_13th_count(2015) == 3
-friday_13th_count(1986) == 1
-friday_13th_count(2019) == 2
+p friday_13th_count(2015) == 3
+p friday_13th_count(1986) == 1
+p friday_13th_count(2019) == 2
