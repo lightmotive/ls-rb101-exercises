00e0aefebbe6bf9090dd6cf4c7c080b25eb517e4
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-11-19 22:46:53 +0000

    Implement process Medium 2 exercise 7 overview

--- 12_medium2/07_unlucky_days.rb
+++ 12_medium2/07_unlucky_days.rb
@@ -6,3 +6,19 @@
 # - Call `select` on range.
 #    - Within `select`, return true when day of month is 13 and day of week
 #      is 5.
+
+def friday_13th_dates(year)
+  # - Initialize two Date objects.
+  #   - Create a range from day 1 through 12/31 of specified year.
+  # - Call `select` on range.
+  #    - Within `select`, return true when day of month is 13 and day of week
+  #      is 5.
+end
+
+def friday_13th_count(year)
+  friday_13th_dates(year).size
+end
+
+friday_13th_count(2015) == 3
+friday_13th_count(1986) == 1
+friday_13th_count(2019) == 2
