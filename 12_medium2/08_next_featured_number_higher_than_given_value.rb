910fd03e36db84d4ce5dbb205d94fd2bf7864185
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-11-21 16:09:51 +0000

    Logically process Medium 2 exercise 8, basic logic

---
+++ 12_medium2/08_next_featured_number_higher_than_given_value.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+# Basic logic
+# - This problem can be solved with simple iteration and condition evaluation.
+#   - Loop to increment the provided number by 1, returning the current number
+#     when all of the following number properties are true (check after
+#     increment):
+#     - Odd
+#     - Multiple of 7
+#     - All digits are unique
