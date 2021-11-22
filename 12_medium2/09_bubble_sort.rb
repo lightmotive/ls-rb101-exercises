1f81f620dfe157e6c6895e27dfc11108d8ea0a9a
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-11-22 00:40:46 +0000

    Explore Medium 2 exercise 9, "Bubble Sort"
    - Implement performance optimization that ignores sorted tail elements.
    - Skip swap if current <= next (instead of skipping only if less than).

--- 12_medium2/09_bubble_sort.rb
+++ 12_medium2/09_bubble_sort.rb
@@ -1,18 +1,24 @@
 # frozen_string_literal: true
 
+# rubocop:disable Metrics/MethodLength
 def bubble_sort!(array)
+  last_index_offset = 2
+
   loop do
     swapped = false
-    0.upto(array.size - 2) do |idx|
-      next if array[idx] < array[idx + 1]
+
+    0.upto(array.size - last_index_offset) do |idx|
+      next if array[idx] <= array[idx + 1]
 
       array[idx], array[idx + 1] = array[idx + 1], array[idx]
       swapped = true
     end
 
+    last_index_offset += 1
     break unless swapped
   end
 end
+# rubocop:enable Metrics/MethodLength
 
 array = [5, 3]
 bubble_sort!(array)
