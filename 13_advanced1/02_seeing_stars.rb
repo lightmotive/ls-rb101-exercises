acdc97426a99f03d7c44d06ad463472e77f30c52
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-11-27 22:38:35 +0000

    Logically process Advanced 1 exercise 2, basic

---
+++ 13_advanced1/02_seeing_stars.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+# Basic logic:
+# Given an odd number:
+# - Print 3 asterisks that are separated by two spaces of equal length.
+# - Repeat until there are no spaces:
+#   - Reduce space size by 1, printing centered content within a length equal
+#     to number.
+# - Print asterisks equal to number.
+# - Reverse the process.
+#   - Create a method that can print outside-in or inside-out (reverse).
+
+def star_top_bottom(number, reverse: false)
+
+end
+
+def star(number)
+  
+end
