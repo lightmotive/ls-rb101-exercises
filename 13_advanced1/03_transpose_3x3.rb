b76add9fc6092bf5ce2a6dfc41e51667661a84ce
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-11-28 00:09:40 +0000

    Complete Advanced 1 exercise 3

--- 13_advanced1/03_transpose_3x3.rb
+++ 13_advanced1/03_transpose_3x3.rb
@@ -23,10 +23,33 @@
 #
 # * A *
 # Given a matrix:
-# - Create matrix_new.
+# - Initialize transposed and assign to empty array.
 # - Counting from 0 to array size - 1:
 #   - Initialize row_transposed to empty array.
 #   - Iterate through matrix: (row)
 #     - Add the element at row[count] to row_transposed.
-#   - Append row_transposed to matrix_new.
-# - Return matrix_new.
+#   - Append row_transposed to transposed.
+# - Return transposed.
+
+def transpose(matrix)
+  transposed = []
+
+  0.upto(matrix.size - 1) do |column_index|
+    row_transposed = []
+    matrix.each { |row| row_transposed.push(row[column_index]) }
+    transposed.push(row_transposed)
+  end
+
+  transposed
+end
+
+matrix = [
+  [1, 5, 8],
+  [4, 7, 2],
+  [3, 9, 6]
+]
+
+new_matrix = transpose(matrix)
+
+p new_matrix == [[1, 4, 3], [5, 7, 9], [8, 2, 6]]
+p matrix == [[1, 5, 8], [4, 7, 2], [3, 9, 6]]
