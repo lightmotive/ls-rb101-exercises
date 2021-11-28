f11e79f53b673a14868af470c3a13d5a04c876cd
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-11-28 00:21:47 +0000

    Explore Advanced 1 exercise 3
    - Logically process creating an in-place transpose! method.

--- 13_advanced1/03_transpose_3x3.rb
+++ 13_advanced1/03_transpose_3x3.rb
@@ -52,3 +52,8 @@ new_matrix = transpose(matrix)
 
 p new_matrix == [[1, 4, 3], [5, 7, 9], [8, 2, 6]]
 p matrix == [[1, 5, 8], [4, 7, 2], [3, 9, 6]]
+
+def transpose!(matrix)
+  # - Create a matrix iterator that represents row_index and column_index.
+  # - Swap matrix[row_index][column_index] with matrix[column_index][row_index].
+end
