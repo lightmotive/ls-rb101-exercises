19487cf71da3d77e055fbf3a6fa9d6af80e570c0
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-12-06 17:58:04 +0000

    Set up Advanced 1 exercise : Fix the Bug

---
+++ 13_advanced1/06_fix_the_bug.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+def my_method(array)
+  if array.empty?
+    []
+  elsif
+    array.map do |value|
+      value * value
+    end
+  else
+    [7 * array.first]
+  end
+end
+
+p my_method([]) == []
+p my_method([3]) == [21]
+p my_method([3, 4]) == [9, 16]
+p my_method([5, 6, 7]) == [25, 36, 49]
