66f56c267e53ba83a285bcca30616c250455e084
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-12-06 18:09:05 +0000

    Logically solve Advanced 1 exercise : Fix the Bug

--- 13_advanced1/06_fix_the_bug.rb
+++ 13_advanced1/06_fix_the_bug.rb
@@ -3,7 +3,7 @@
 def my_method(array)
   if array.empty?
     []
-  elsif
+  elsif 
     array.map do |value|
       value * value
     end
@@ -13,6 +13,17 @@ def my_method(array)
 end
 
 p my_method([]) == []
-p my_method([3]) == [21]
-p my_method([3, 4]) == [9, 16]
-p my_method([5, 6, 7]) == [25, 36, 49]
+p my_method([3]) #== [21]
+p my_method([3, 4]) #== [9, 16]
+p my_method([5, 6, 7]) #== [25, 36, 49]
+
+# `my_method` has an empty `elsif` as part of the `if` statement. One could
+# also interpret that as if Ruby interprets the body of the elsif statement as
+# the condition, which will be a truthy value as long as the `array` variable is
+# not nil or false. If it were, the program would throw an error. Therefore,
+# the program always returns `nil` when a non-empty Array object is provided.
+#
+# One needs to look at the expected output to determine how to fix the method:
+# - When an array with one element is passed in, return 7 * the first element.
+# - When an array with multiple elements is passed in, square each array
+#   element of the array (transformation).
