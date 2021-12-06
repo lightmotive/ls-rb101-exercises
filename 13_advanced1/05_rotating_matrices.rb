b60d73f7d3721978c5006237a197f120e2831070
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-12-06 15:45:22 +0000

    Performance test Advanced 1 exercise 5
    - `map_reverse` was faster than `rotate_reverse_ref` by 1.07x average.

--- 13_advanced1/05_rotating_matrices.rb
+++ 13_advanced1/05_rotating_matrices.rb
@@ -4,7 +4,7 @@
 # *P*
 # Input: Array of arrays (matrix)
 # Output: The same array, rotated 90 degrees clockwise
-# Metal model: To rotate a matrix 90 degrees, convert each row into columns.
+# Metal model: To rotate_reverse_ref a matrix 90 degrees, convert each row into columns.
 #   The last row becomes the first column, the second to last row becomes the
 #   second column, and so on.
 #
@@ -24,7 +24,7 @@
 
 # C (Implementation, to be coded with intent)
 
-def rotate(matrix, rotations: 1)
+def rotate_reverse_ref(matrix, rotations = 1)
   rotated = []
 
   (matrix.size - 1).downto(0) do |row_idx|
@@ -36,8 +36,28 @@ def rotate(matrix, rotations: 1)
     end
   end
 
-  rotations == 1 ? rotated : rotate(rotated, rotations: rotations - 1)
+  rotations == 1 ? rotated : rotate_reverse_ref(rotated, rotations - 1)
 end
+
+# Launch School solution:
+def rotate_map_reverse(matrix, rotations = 1)
+  result = []
+  number_of_rows = matrix.size
+  number_of_columns = matrix.first.size
+
+  (0...number_of_columns).each do |column_index|
+    new_row = (0...number_of_rows).map do |row_index|
+      matrix[row_index][column_index]
+    end
+
+    result << new_row.reverse
+  end
+
+  rotations == 1 ? result : rotate_map_reverse(result, rotations - 1)
+end
+
+require_relative '../../ruby-common/test'
+require_relative '../../ruby-common/benchmark_report'
 
 matrix1 = [
   [1, 5, 8],
@@ -50,10 +70,31 @@ matrix2 = [
   [5, 1, 0, 8]
 ]
 
-new_matrix1 = rotate(matrix1)
-new_matrix2 = rotate(matrix2)
-new_matrix3 = rotate(matrix2, rotations: 4)
+TESTS = [
+  {
+    input: [matrix1],
+    expected_output: [[3, 4, 1], [9, 7, 5], [6, 2, 8]]
+  },
+  {
+    input: [matrix2],
+    expected_output: [[5, 3], [1, 7], [0, 4], [8, 2]]
+  },
+  {
+    input: [matrix2, 4],
+    expected_output: matrix2
+  }
+].freeze
 
-p new_matrix1 == [[3, 4, 1], [9, 7, 5], [6, 2, 8]]
-p new_matrix2 == [[5, 3], [1, 7], [0, 4], [8, 2]]
-p new_matrix3 == matrix2
+run_tests('rotate_reverse_ref', TESTS, ->(input) { rotate_reverse_ref(*input) })
+run_tests('rotate_map_reverse', TESTS, ->(input) { rotate_map_reverse(*input) })
+
+benchmark_report(
+  5, 500, TESTS,
+  [
+    { label: 'reverse_ref', method: ->(input) { rotate_reverse_ref(*input) } },
+    { label: 'map_reverse', method: ->(input) { rotate_map_reverse(*input) } }
+  ]
+)
+
+# Once again, the idiomatic Ruby approach is slightly faster (1.07x avg) than
+# a procedural approach!
