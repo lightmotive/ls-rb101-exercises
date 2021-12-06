f8bb8d79a2d15d9590f9393db06d3a6a4df384f1
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-12-06 16:04:52 +0000

    Understand Launch School solution algorithm
    - Try to improve with map that doesn't require reverse.

--- 13_advanced1/05_rotating_matrices.rb
+++ 13_advanced1/05_rotating_matrices.rb
@@ -40,6 +40,15 @@ def rotate_reverse_ref(matrix, rotations = 1)
 end
 
 # Launch School solution:
+# *A*
+# Given a matrix:
+#
+# - Initialize result as an array to contain the rotated matrix
+# - Iterate through matrix column indices
+#   - Iterate through matrix row indices, mapping the matrix column row by row
+#     to new_row.
+#   - Append reversed new_row to result.
+# - Return result
 def rotate_map_reverse(matrix, rotations = 1)
   result = []
   number_of_rows = matrix.size
@@ -51,6 +60,31 @@ def rotate_map_reverse(matrix, rotations = 1)
     end
 
     result << new_row.reverse
+  end
+
+  rotations == 1 ? result : rotate_map_reverse(result, rotations - 1)
+end
+
+# *A*
+# Alternative map_reverse that obviates need for row.reverse:
+#
+# - Initialize result as an array to contain the rotated matrix
+# - Iterate through matrix column indices
+#   - Iterate through matrix row indices in reverse, mapping the matrix column
+#     row by row to new_row.
+#   - Append new_row to result.
+# - Return result
+def rotate_map(matrix, rotations = 1)
+  result = []
+  number_of_rows = matrix.size
+  number_of_columns = matrix.first.size
+
+  (0...number_of_columns).each do |column_index|
+    new_row = (-(number_of_rows - 1)..0).map do |row_index|
+      matrix[row_index][column_index]
+    end
+
+    result << new_row
   end
 
   rotations == 1 ? result : rotate_map_reverse(result, rotations - 1)
@@ -87,14 +121,19 @@ TESTS = [
 
 run_tests('rotate_reverse_ref', TESTS, ->(input) { rotate_reverse_ref(*input) })
 run_tests('rotate_map_reverse', TESTS, ->(input) { rotate_map_reverse(*input) })
+run_tests('rotate_map', TESTS, ->(input) { rotate_map(*input) })
 
 benchmark_report(
   5, 500, TESTS,
   [
     { label: 'reverse_ref', method: ->(input) { rotate_reverse_ref(*input) } },
-    { label: 'map_reverse', method: ->(input) { rotate_map_reverse(*input) } }
+    { label: 'map_reverse', method: ->(input) { rotate_map_reverse(*input) } },
+    { label: 'map', method: ->(input) { rotate_map(*input) } }
   ]
 )
 
 # Once again, the idiomatic Ruby approach is slightly faster (1.07x avg) than
 # a procedural approach!
+
+# The new rotate_map method is slightly faster than map_reverse most of the
+# time. Is it consistently faster with larger arrays? Let's try...
