ffb882bb509af02aed05d225eb318bebb84af6be
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-12-06 18:34:33 +0000

    Logically process Advanced 1 exercise 7, [PEDA]C

---
+++ 13_advanced1/07_merge_sorted_lists.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+# **PEDAC**
+
+# *P*
+# Input: Two sorted arrays.
+# Output: One array that includes the elements of both arrays in sorted order.
+#
+# Rules:
+# - Do not concatenate the arrays and then sort.
+#
+# Mental model: Combine the elements of each array so the elements are sorted
+# at all times.
+
+# *E*
+# merge([1, 5, 9], [2, 6, 8]) == [1, 2, 5, 6, 8, 9]
+# merge([1, 1, 3], [2, 2]) == [1, 1, 2, 2, 3]
+# merge([], [1, 4, 5]) == [1, 4, 5]
+# merge([1, 4, 5], []) == [1, 4, 5]
+
+# *D*
+# Two input arrays and one new array to hold the sorted elements.
+
+# *A*
+# Given two sorted arrays called array1 and array2:
+#
+# - Initialize result and assign to a new array.
+# - Track the current comparison position of each array using variables named
+#   array1_position and array2_position variables. Both start at 0.
+# - While both position variables are less than the last index of their
+#   associated arrays:
+#   - Compare the elements at the current comparison position of each array.
+#     - Add the smallest or first equal element to result, incrementing the
+#       position tracker for the associated array from which the element was
+#       copied.
+# - Return result.
