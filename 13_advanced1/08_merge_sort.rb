d237bb6f3d1aaecb5580a01efad3708a5b267f00
Andre Light <andrelight.al@gmail.com>
2022-01-22 01:08:01 +0000

    Reverse-engineer the algorithm using [PEDA]C

--- 13_advanced1/08_merge_sort.rb
+++ 13_advanced1/08_merge_sort.rb
@@ -112,17 +112,84 @@ def merge_sort(array)
 end
 
 # Further exploration: perform merge sort non-recursively ("bottom-up").
+# TODO: fill in PED of PEDAC...
+# *P*
+# Sort an array of any size using an existing `merge` function that accepts two
+# sorted array arguments and returns a sorted array.
+# - Return a new array (don't mutate original).
+# - Don't use recursion.
+
+# *E*
+# Examples and test cases are the same as for Recursive Merge Sort above.
+
+# *D*
+# - Duplicate the original array to prevent mutation.
+# - Select sub-arrays to pass to the merge function, then merge the result
+#   in-place.
+
 # *A*
-# Src: https://algorithmist.com/wiki/Merge_sort
-# Input: array a[] indexed from 0 to n-1.
+# Original source: https://algorithmist.com/wiki/Merge_sort
 #
-# m = 1
-# while m < n do
-#     i = 0
-#     while i < n-m do
-#         merge subarrays a[i..i+m-1] and a[i+m .. min(i+2*m-1,n-1)] in-place.
-#         i = i + 2 * m
-#     m = m * 2
+# Clarified algorithm based on original source:
+# Overview:
+# - Start with a function that merges two sorted arrays, which can be
+#   the same function one uses at the end of a recursive merge sort method.
+# - The bottom-up merge sort will merge an array in-place, though the merge
+#   function creates a new array to hold the sorted elements from each sorted
+#   array.
+# - Start with a merge size of 1.
+# - Pass through the entire array, selecting two adjacent arrays of size
+#   [merge size], passing those arrays to the `merge` function.
+#   - That returns a single array with the elements merged.
+#   - After the first pass, each set of 2 elements from left to right will be
+#     sorted.
+# - Increase the merge size by multiplying the previous merge size by 2.
+# - Continue passing through the array, selecting and merging adjacent sets
+#   of sorted arrays.
+
+# Details:
+# **Sub-procedure: merge_in_place!(array, merge_start_idx, merge_size)
+# - Set `merge_middle` to the start index + sub-array size that's sorted.
+#   - I.e., `merge_start_idx + merge_size`.
+# - Set `merge_end` to the smaller of either:
+#   - Start index through the last index of two adjacent sorted sub-arrays
+#     - I.e., `merge_start_idx + (merge_size * 2) - 1`
+#   - [OR] Start index through the last element index
+#     - I.e., `array.size - 1`
+# - Set `first_set` to an `array` sub-array that includes the range from
+#   start index up to (excluding) `merge_middle`.
+# - Set `second_set` to an `array` sub-array that includes the range from
+#   `merge_middle` through (including) `merge_end`.
+# - Update the `array` sub-array from `merge_start_idx` through `merge_end` to
+#   the result of `merge(first_set, second_set)` (in-place; `array` is mutated).
+#
+# **Sub-procedure: merge_pass!(array, merge_size)**
+# - Set `merge_start_idx` to 0
+# - While `merge_start_idx` is less than array.size minus `merge_size`:
+#   - Using merge_in_place!, merge two sub-arrays starting at `merge_start_idx`
+#     through `merge_size` in-place.
+#   - Increase `merge_start_idx` by 2 * `merge_size`
+# end
+#
+# **Main procedure: merge_sort_bottom_up(array)**
+# - Reassign array parameter to a variable named `array` to prevent original
+#   array mutation.
+# - Set `merge_size` to 1.
+# - While merge_size < array.size
+#   - Using `merge_pass!`, pass through the array to merge sub-array sets of
+#     size [merge_size].
+#   - Increase merge_size by merge_size * 2.
+# - Return `array`.
+
+# Input: array indexed from 0 to array_size-1.
+#
+# merge_size = 1
+# while merge_size < array_size do
+#     merge_start_idx = 0
+#     while merge_start_idx < array_size-merge_size do
+#         merge subarrays array[merge_start_idx..merge_start_idx+merge_size-1] and array[merge_start_idx+merge_size .. min(merge_start_idx+2*merge_size-1,array_size-1)] in-place.
+#         merge_start_idx = merge_start_idx + 2 * merge_size
+#     merge_size = merge_size * 2
 
 def merge_in_place!(array, merge_start_idx, merge_size)
   merge_middle = merge_start_idx + merge_size
@@ -160,6 +227,12 @@ end
 require_relative '../../ruby-common/test'
 require_relative '../../ruby-common/benchmark_report'
 
+def sorted_big_array_create(element_count)
+  (1..element_count).map { rand(element_count) }
+end
+
+big_array = sorted_big_array_create(50_000)
+
 TESTS = [
   { input: [9, 5, 7, 1], expected_output: [1, 5, 7, 9] },
   { input: [5, 3], expected_output: [3, 5] },
@@ -169,21 +242,26 @@ TESTS = [
   { input: [7, 3, 9, 15, 23, 1, 6, 51, 22,
             37, 54, 43, 5, 25, 35, 18, 46],
     expected_output: [1, 3, 5, 6, 7, 9, 15, 18, 22,
-                      23, 25, 35, 37, 43, 46, 51, 54] }
-  # , { input: [big_array1, big_array2],
-  #   expected_output: merge_multiple_passes(big_array1, big_array2) }
+                      23, 25, 35, 37, 43, 46, 51, 54] },
+  { input: big_array, expected_output: merge_sort_bottom_up(big_array) }
 ].freeze
 
-run_tests('merge_sort_recursive', TESTS, ->(input) { merge_sort(input) })
 run_tests('merge_sort_bottom_up', TESTS,
           ->(input) { merge_sort_bottom_up(input) })
+run_tests('merge_sort_recursive', TESTS, ->(input) { merge_sort(input) })
 
 benchmark_report(
-  2, 200, TESTS,
+  1, 2, TESTS,
   [
-    { label: 'merge_sort_recursive',
-      method: ->(input) { merge_sort(input) } },
     { label: 'merge_sort_bottom_up',
-      method: ->(input) { merge_sort_bottom_up(input) } }
+      method: ->(input) { merge_sort_bottom_up(input) } },
+    { label: 'merge_sort_recursive',
+      method: ->(input) { merge_sort(input) } }
   ]
 )
+
+# Key points learned:
+# - The bottom-up algorithm is probably not optimized because it's slower than
+#   the recursive approach.
+# - The recursive approach is easier to understand and maintain as long as one
+#   understands recursion basics.
