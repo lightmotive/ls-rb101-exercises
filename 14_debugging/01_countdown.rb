90fe6977d934ac6c345af591d49a34d441a952e1
Andre Light <andrelight.al@gmail.com>
2022-02-02 00:16:54 +0000

    Complete Debugging exercise 01: Coundown

--- 14_debugging/01_countdown.rb
+++ 14_debugging/01_countdown.rb
@@ -11,7 +11,14 @@ counter = 10
 
 10.times do
   puts counter
-  decrease(counter)
+  # decrease(counter)
+  # The problem is with line 14 above: `counter` is not decremented
+  # because of 2 reasons:
+  # - `counter`'s value is an Integer, which is immutable.
+  # - Even if `counter` was a mutable object, the `decrease` method
+  #   doesn't mutate the object; it reassigns it to a decremented value.
+  # Changing the line to this will fix the issue:
+  counter = decrease(counter)
 end
 
 puts 'LAUNCH!'
