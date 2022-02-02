91d272a18e3adc9e6e9d10331134e6d6a6917ee0
Andre Light <andrelight.al@gmail.com>
2022-02-02 00:33:30 +0000

    Complete Debugging exercise 02: HEY YOU!

--- 14_debugging/02_hey_you.rb
+++ 14_debugging/02_hey_you.rb
@@ -5,9 +5,19 @@
 # expected output.
 
 def shout_out_to(name)
-  name.chars.each { |c| c.upcase! }
+  # The problem: `name.chars` returns an array of chars. Each char in the array
+  # points to a different string object, none of which point to the underlying
+  # value in the `name` String object:
+  # name.chars.each { |c| c.upcase! }
 
-  puts 'HEY ' + name
+  # There are at least 2 ways to fix the issue:
+  # 1. Simplify by having `name` invoke **String#upcase** directly (usually
+  #    best to avoid mutation/side effects).
+  # 2. `name = name.chars.map(&:upcase).join`
+
+  # We'll fix this with option 1.
+
+  puts "HEY #{name.upcase}"
 end
 
 shout_out_to('you') # expected: 'HEY YOU'
