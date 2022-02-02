89cf2d12eb842cd9e1401c6d2821e3a292018caf
Andre Light <andrelight.al@gmail.com>
2022-02-02 00:24:52 +0000

    Initialize Debugging exercise 02: HEY YOU!

--- 14_debugging/02_hey_you.rb
+++ 14_debugging/02_hey_you.rb
@@ -1,1 +1,13 @@
 # frozen_string_literal: true
+
+# String#upcase! is a destructive method, so why does this code print
+# *HEY you* instead of *HEY YOU*? Modify the code so that it produces the
+# expected output.
+
+def shout_out_to(name)
+  name.chars.each { |c| c.upcase! }
+
+  puts 'HEY ' + name
+end
+
+shout_out_to('you') # expected: 'HEY YOU'
