3a0721015e7429373a7a10ffbd39663816482222
Andre Light <andrelight.al@gmail.com>
2022-02-04 00:28:52 +0000

    Explain Debugging exercise 07: Neutralizer

--- 14_debugging/07_neutralizer.rb
+++ 14_debugging/07_neutralizer.rb
@@ -6,6 +6,21 @@
 def neutralize(sentence)
   words = sentence.split(' ')
   words.each do |word|
+    # The problem: the following line mutates the array during iteration,
+    # which causes the iteration to miss the word *boring* after *dull* is
+    # deleted.
+    # What actually happens, in a nutshell, is this:
+    #   - `each` iterates through each word in `words`, passing the word to
+    #     the block while keeping track of the current index within the method.
+    #     - `each` passes *dull* to the block, which results in executing 
+    #       `words.delete('dull')` because `negative?(word)` returns `true`.
+    #       - Behind the scenes, `each` is on index 1.
+    #       - `words` now looks like this: `%w[These boring cards ...]`;
+    #         *boring* is now at the same index at which the just-deleted *dull*
+    #         word was before deletion: index 1
+    #     - `each` increments the internal index to 2, which points to *cards*
+    #       in `words`. That's why *boring* is missed--it's never evaluated due
+    #       to the mutation
     words.delete(word) if negative?(word)
   end
 
