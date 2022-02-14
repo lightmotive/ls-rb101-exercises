4e463a3a2ba84c4e215d2036bd20ccc5a9ab0d2c
Andre Light <andrelight.al@gmail.com>
2022-02-14 20:32:48 +0000

    Initialize Debugging exercise 11
    - "What's wrong with the output?"

--- 14_debugging/11_whats_wrong_with_the_output.rb
+++ 14_debugging/11_whats_wrong_with_the_output.rb
@@ -1,1 +1,13 @@
 # frozen_string_literal: true
+
+# Josh wants to print an array of numeric strings in reverse numerical order.
+# However, the output is wrong. Without removing any code, help Josh get the
+# expected output.
+
+arr = %w[9 8 7 10 11]
+p arr.sort do |x, y|
+  y.to_i <=> x.to_i
+end
+
+# Expected output: ["11", "10", "9", "8", "7"]
+# Actual output: ["10", "11", "7", "8", "9"]
