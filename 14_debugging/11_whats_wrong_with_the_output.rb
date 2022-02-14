110eea2f465dd799bf39d29431383fe78a9bdce4
Andre Light <andrelight.al@gmail.com>
2022-02-14 20:38:09 +0000

    Complete Debugging exercise 11

--- 14_debugging/11_whats_wrong_with_the_output.rb
+++ 14_debugging/11_whats_wrong_with_the_output.rb
@@ -5,9 +5,14 @@
 # expected output.
 
 arr = %w[9 8 7 10 11]
-p arr.sort do |x, y|
+
+# The problem: `p` binds more tightly with `arr.sort` than `arr.sort` does
+# with the `do...end` block. Therefore, `(p arr.sort)` is executed with the
+# block, which essentially ignores the block.
+# The solution: wrap `arr.sort do...end` in parenthese.
+p(arr.sort do |x, y|
   y.to_i <=> x.to_i
-end
+end)
 
 # Expected output: ["11", "10", "9", "8", "7"]
 # Actual output: ["10", "11", "7", "8", "9"]
