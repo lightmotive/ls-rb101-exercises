877548ff7c6184486bcf65cbc8dd61c02cfd2b37
Andre Light <andrelight.al@gmail.com>
2022-02-25 14:52:58 +0000

    Second attempt of Easy 1, Exercise 4: How Many?
    - Write simple algorithm.

---
+++ 02_easy1/04.2_how_many.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+# Simple algorithm for minimum iterations:
+# - Create a hash with a string key and an integer value.
+# - Loop through each array `string`.
+#   - Access the Hash at `string` and increment the value.
