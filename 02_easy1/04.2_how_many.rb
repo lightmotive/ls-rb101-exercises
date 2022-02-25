3198ee317cdba9b3a8a04b1d46c5049b090dfda7
Andre Light <andrelight.al@gmail.com>
2022-02-25 15:06:08 +0000

    Second attempt of Easy 1, Exercise 4: How Many?
    - Implement.

--- 02_easy1/04.2_how_many.rb
+++ 02_easy1/04.2_how_many.rb
@@ -1,6 +1,46 @@
 # frozen_string_literal: true
 
 # Simple algorithm for minimum iterations:
-# - Create a hash with a string key and an integer value.
-# - Loop through each array `string`.
-#   - Access the Hash at `string` and increment the value.
+# - Create a hash with a string key and a default integer value of 0.
+# - Loop through each array `word`.
+#   - Access the Hash at `word` and increment the value.
+
+vehicles = %w[
+  car car truck car SUV truck
+  motorcycle motorcycle car truck
+]
+
+def occurrences_count(words)
+  map = Hash.new(0)
+
+  words.each { |word| map[word] += 1 }
+
+  map
+end
+
+p occurrences_count(vehicles) == {
+  'car' => 4,
+  'truck' => 3,
+  'SUV' => 1,
+  'motorcycle' => 2
+}
+
+def occurrences_as_string(occurrences_count)
+  string = String.new
+
+  occurrences_count.each do |occurrence, count|
+    string << "#{occurrence} => #{count}\n"
+  end
+
+  string
+end
+
+vehicles_as_string = occurrences_as_string(occurrences_count(vehicles))
+p vehicles_as_string == <<~OUT
+  car => 4
+  truck => 3
+  SUV => 1
+  motorcycle => 2
+OUT
+
+puts vehicles_as_string
