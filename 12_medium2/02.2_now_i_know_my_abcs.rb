040352a730e4e8c6a2259a5a6e8e8e4ea12c2cd5
Andre Light <andrelight.al@gmail.com>
2022-02-28 20:42:08 +0000

    Second attempt of Medium 2, Exercise 2
    - "Now I Know My ABCs"
    - Initialize problem.

---
+++ 12_medium2/02.2_now_i_know_my_abcs.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+# A collection of spelling blocks has two letters per block, as shown in this
+# list:
+#
+# B:O   X:K   D:Q   C:P   N:A
+# G:T   R:E   F:S   J:W   H:U
+# V:I   L:Y   Z:M
+
+# This limits the words you can spell with the blocks to just those words that
+# do not use both letters from any given block. Each block can only be used
+# once.
+#
+# Write a method that returns true if the word passed in as an argument can be
+# spelled from this set of blocks, false otherwise.
+#
+# Examples:
+block_word?('BATCH') == true
+block_word?('BUTCH') == false
+block_word?('jest') == true
