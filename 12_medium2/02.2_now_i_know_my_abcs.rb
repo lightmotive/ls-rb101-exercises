4f253b7381f50151ec66479785ac12c5b1626d73
Andre Light <andrelight.al@gmail.com>
2022-02-28 20:46:52 +0000

    Second attempt of Medium 2, Exercise 2
    - Process: [PE]DAC

--- 12_medium2/02.2_now_i_know_my_abcs.rb
+++ 12_medium2/02.2_now_i_know_my_abcs.rb
@@ -6,15 +6,22 @@
 # B:O   X:K   D:Q   C:P   N:A
 # G:T   R:E   F:S   J:W   H:U
 # V:I   L:Y   Z:M
-
+#
 # This limits the words you can spell with the blocks to just those words that
 # do not use both letters from any given block. Each block can only be used
 # once.
 #
 # Write a method that returns true if the word passed in as an argument can be
 # spelled from this set of blocks, false otherwise.
-#
+
+# Input: String
+# Output: Boolean
+
+# Mental model:
+# Given a set of letter pairs, check whether a string can be spelled with no
+# more than one letter from each pair.
+
 # Examples:
-block_word?('BATCH') == true
-block_word?('BUTCH') == false
-block_word?('jest') == true
+p block_word?('BATCH') == true
+p block_word?('BUTCH') == false
+p block_word?('jest') == true
