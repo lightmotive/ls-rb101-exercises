074e963f76fc577dde9dbad118755c8add3c228d
Andre Light <andrelight.al@gmail.com>
2022-03-05 00:55:37 +0000

    Personal practice: Combinations
    - Translate Python recursive solution.

--- 15_personal_practice/combination.rb
+++ 15_personal_practice/combination.rb
@@ -3,87 +3,62 @@
 # Write a Ruby-flavored algorithm that generates an array of unique combinations
 # of `n` elements within a given array, then implement.
 
-# combination([12, 13, 7, 5], 2) | array, c_count
+# Implementation adapted from this source: https://www.geeksforgeeks.org/print-all-possible-combinations-of-r-elements-in-a-given-array-of-size-n/
+# Due to time constraints, I decided not to fully understand this algorithm
+# at a deep level. This is a solved problem for which I see no need to solve
+# again at this point in my career.
 
-# Values   -- Indices
-# ******   -- *******
-# [12, 13] -- [0, 1, -, -]
-# [12, 7]  -- [0, -, 2, -]
-# [12, 5]  -- [0, -, -, 3]
-# [13, 7]  -- [-, 1, 2, -]
-# [13, 5]  -- [-, 1, -, 3]
-# [7, 5]   -- [-, -, 2, 3] -- Iteration ends
-# ***
-# combos = []
-# middle_size = c_count - 1
-# (0..(array.size - c_count)): start_idx
-#   ((start_idx + 1)...(array.size - middle_size)): middle_start_idx
-#     combos << (array[start_idx] + array[middle_start_idx, middle_size])
+# rubocop:disable Metrics/ParameterLists
+def combination_recurse!(input_array, combo, input_start_idx, input_end_idx,
+                         combo_idx, c_size, combos)
+  return combos << combo.dup if combo_idx == c_size
 
-# combination([12, 13, 7, 5], 3) | array, c_count
-
-# start_idx = 0
-# [12, 13, 7] -- [0, 1, 2, -]
-# [12, 13, 5] -- [0, 1, -, 3]
-# [12, 7, 5]  -- [0, -, 2, 3]
-# start_idx = 1
-# [13, 7, 5]  -- [-, 1, 2, 3]
-# ***
-# middle_size = c_count - 2
-# (0..(array.size - c_count)): start_idx
-#   1.upto(middle_offset_max): middle_offset (possible middle starting points, with a minimum of 1)
-#     ((start_idx + middle_offset + middle_size)...array.size): end_idx
-#       combos << ([array[start_idx]] + array[start_idx + middle_offset, middle_size] + [array[end_idx]])
-
-def combination_middle_offset_max(array, c_count)
-  return 1 if c_count < 3
-
-  middle_size = c_count - 2
-
-  array.size - middle_size - 1
-end
-
-def combos_from_start(array, c_count, start_idx)
-  combos = []
-  middle_size = c_count - 2
-  middle_offset_max = combination_middle_offset_max(array, c_count)
-
-  1.upto(middle_offset_max) do |middle_offset|
-    ((start_idx + middle_offset + middle_size)...array.size).each do |end_idx|
-      p [start_idx, "#{start_idx + middle_offset}, #{middle_size}", end_idx]
-      combos << ([array[start_idx]] + array[start_idx + middle_offset, middle_size] + [array[end_idx]])
-    end
+  # Replace combo_idx with all possible elements.
+  # The condition "input_end_idx-idx+1 >= c_size-combo_idx" makes sure that including one
+  # element at combo_idx will make a combination with remaining elements at
+  # remaining positions.
+  idx = input_start_idx
+  while (idx <= input_end_idx) &&
+        ((input_end_idx - idx + 1) >= (c_size - combo_idx))
+    combo[combo_idx] = input_array[idx]
+    combination_recurse!(input_array, combo, idx + 1, input_end_idx,
+                         combo_idx + 1, c_size, combos)
+    idx += 1
   end
+end
+# rubocop:enable Metrics/ParameterLists
+
+def combination(input_array, c_size)
+  combo = [0] * c_size
+  combos = []
+
+  combination_recurse!(input_array, combo, 0, input_array.size - 1, 0, c_size, combos)
 
   combos
 end
 
-def combination(array, c_count)
-  combos = []
-  # middle_offset_max = combination_middle_offset_max(array, c_count)
+p combination([12, 13, 7, 5], 2) == [[12, 13], [12, 7], [12, 5], [13, 7], [13, 5], [7, 5]]
+p combination([12, 13, 7, 5], 3) == [[12, 13, 7], [12, 13, 5], [12, 7, 5], [13, 7, 5]]
 
-  (0..(array.size - c_count)).each do |start_idx|
-    combos.append(*combos_from_start(array, c_count, start_idx))
-  end
+p combination([12, 13, 7, 5, 10], 2) == [[12, 13], [12, 7], [12, 5], [12, 10], [13, 7], [13, 5], [13, 10], [7, 5],
+                                         [7, 10], [5, 10]]
 
-  combos
-end
+p combination([12, 13, 7, 5, 10], 3) == [[12, 13, 7], [12, 13, 5], [12, 13, 10], [12, 7, 5], [12, 7, 10], [12, 5, 10],
+                                         [13, 7, 5], [13, 7, 10], [13, 5, 10], [7, 5, 10]]
 
-# p combination([12, 13, 7, 5], 2).sort == [[12, 13], [12, 7], [12, 5], [13, 7], [13, 5], [7, 5]].sort
-# p combination([12, 13, 7, 5], 3).sort == [[12, 13, 7], [12, 13, 5], [12, 7, 5], [13, 7, 5]].sort
-# p combination([12, 13, 7, 5], 4).sort == [[12, 13, 7, 5]].sort
-p combination([12, 13, 7, 5, 10, 16], 4).sort #== [[12, 13, 7, 5],
-#                                                   [12, 13, 7, 10],
-#                                                   [12, 13, 7, 16],
-#                                                   [12, 13, 5, 10],
-#                                                   [12, 13, 5, 16],
-#                                                   [12, 13, 10, 16],
-#                                                   [12, 7, 5, 10],
-#                                                   [12, 7, 5, 16],
-#                                                   [12, 7, 10, 16],
-#                                                   [12, 5, 10, 16],
-#                                                   [13, 7, 5, 10],
-#                                                   [13, 7, 5, 16],
-#                                                   [13, 7, 10, 16],
-#                                                   [13, 5, 10, 16],
-#                                                   [7, 5, 10, 16]].sort
+p combination([12, 13, 7, 5], 4) == [[12, 13, 7, 5]]
+
+p combination([12, 13, 7, 5, 10, 16], 4) == [[12, 13, 7, 5], [12, 13, 7, 10], [12, 13, 7, 16], [12, 13, 5, 10],
+                                             [12, 13, 5, 16], [12, 13, 10, 16], [12, 7, 5, 10], [12, 7, 5, 16],
+                                             [12, 7, 10, 16], [12, 5, 10, 16], [13, 7, 5, 10], [13, 7, 5, 16],
+                                             [13, 7, 10, 16], [13, 5, 10, 16], [7, 5, 10, 16]]
+
+p combination([12, 13, 7, 5, 10, 16, 21], 4) == [[12, 13, 7, 5], [12, 13, 7, 10], [12, 13, 7, 16], [12, 13, 7, 21],
+                                                 [12, 13, 5, 10], [12, 13, 5, 16], [12, 13, 5, 21], [12, 13, 10, 16],
+                                                 [12, 13, 10, 21], [12, 13, 16, 21], [12, 7, 5, 10], [12, 7, 5, 16],
+                                                 [12, 7, 5, 21], [12, 7, 10, 16], [12, 7, 10, 21], [12, 7, 16, 21],
+                                                 [12, 5, 10, 16], [12, 5, 10, 21], [12, 5, 16, 21], [12, 10, 16, 21],
+                                                 [13, 7, 5, 10], [13, 7, 5, 16], [13, 7, 5, 21], [13, 7, 10, 16],
+                                                 [13, 7, 10, 21], [13, 7, 16, 21], [13, 5, 10, 16], [13, 5, 10, 21],
+                                                 [13, 5, 16, 21], [13, 10, 16, 21], [7, 5, 10, 16], [7, 5, 10, 21],
+                                                 [7, 5, 16, 21], [7, 10, 16, 21], [5, 10, 16, 21]]
