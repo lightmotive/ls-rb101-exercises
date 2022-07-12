f07d3be7a7901be00db27c7aa71f85ed33e3ab50
Andre Light <andrelight.al@gmail.com>
2022-07-12 09:16:40 +0000

    Add enumeration w/ block demonstration

--- 15_personal_practice/02_combination_recursive.rb
+++ 15_personal_practice/02_combination_recursive.rb
@@ -130,6 +130,9 @@ def example_combination_enumeration(arr, c_size)
     puts 'No more combinations.'
     break
   end
+
+  puts '* All combinations *'
+  ArrayCustom.new(arr).combination_indices(c_size) { |combo| p combo }
 end
 
 # example_combination_enumeration([12, 13, 7, 5, 10, 16], 4)
