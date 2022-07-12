4846655e501050e67dfe256a0edd968bb125c04a
Andre Light <andrelight.al@gmail.com>
2022-07-12 02:25:09 +0000

    Algorithm verified

--- 15_personal_practice/02_combination_recursive.rb
+++ 15_personal_practice/02_combination_recursive.rb
@@ -77,21 +77,42 @@ p combination([12, 13, 7, 5, 10, 16, 21], 4) == [[12, 13, 7, 5], [12, 13, 7, 10]
 - Iterate through possible 1st indices:
   (0..arr_size - c_size) |start_idx| [0, 1, 2]
   - Iterate through possible 2nd indices:
-    ((start_idx + 1)..arr_size - c_size + 1) |idx_offset1|
+    ((start_idx + 1)..arr_size - c_size + 1 - start_idx) |idx_offset1|
     - Iterate through possible 3rd indices:
-      ((idx_offset1 + 1)..arr_size - c_size + 2) |idx_offset2|
+      ((idx_offset1 + 1)..arr_size - c_size + 2 - start_idx) |idx_offset2|
       - Iterate through possible last indices:
-        ((idx_offset2 + 1)..[arr_size - c_size + 3, arr_size - 1].max) |idx_offset_last|
+        ((idx_offset2 + 1)..[arr_size - c_size + 3 - start_idx, arr_size - 1].max) |idx_offset_last|
         - yield combo[start_idx, idx_offset1, idx_offset2, idx_offset_last]
 
 Given arr = [0, 1, 2, 3, 4, 5] and c_size = 4
-(0..2).each do |start_idx|
-  (1..3).each do |idx_offset1|
-    (2..4).each do |idx_offset2|
-      (3..5).each do |idx_offset_last|
-        - start_idx = 0
+- start_idx = 0
+    (1..3).each do |idx_offset1|
+      (2..4).each do |idx_offset2|
+        (3..5).each do |idx_offset_last|
           - yield [0, 1, 2, 3]
-
+          - yield [0, 1, 2, 4]
+          - yield [0, 1, 2, 5]
+            - yield [0, 1, 3, 4]
+            - yield [0, 1, 3, 5]
+              - yield [0, 1, 4, 5]
+                - yield [0, 2, 3, 4]
+                - yield [0, 2, 3, 5]
+                  - yield [0, 2, 4, 5]
+                    - yield [0, 3, 4, 5]
+- start_idx = 1
+    (2..3).each do |idx_offset1|
+      (3..4).each do |idx_offset2|
+        (4..5).each do |idx_offset_last|
+          - yield [1, 2, 3, 4]
+          - yield [1, 2, 3, 5]
+            - yield [1, 2, 4, 5]
+              - yield [1, 3, 4, 5]
+- start_idx = 2
+    (3..3).each do |idx_offset1|
+      (4..4).each do |idx_offset2|
+        (5..5).each do |idx_offset_last|
+          - yield [2, 3, 4, 5]
+          
 
 
 def combination_indices(start_idx, arr_size, c_size, combo = [])
